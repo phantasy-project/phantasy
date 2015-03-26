7e980e7f01a0326b8e4ac7fee57eaa7252a797d0
Dylan Maxwell <maxwelld@frib.msu.edu>
2015-03-26 12:02:00 -0400

    Improve settings parser to support FS1 elements. General cleanup and improved logging support.

--- phyutil/machine/frib/settings/impact.py
+++ phyutil/machine/frib/settings/impact.py
@@ -7,15 +7,24 @@ __copyright__ = "Copyright (c) 2015, Facility for Rare Isotope Beams"
 __author__ = "Dylan Maxwell"
 
 
-import os
+import os, logging
 
 from collections import OrderedDict
 
 from ....phylib.layout.accel import *
 
 
-def build_settings(accel, latpath, start=None, end=None):
+_LOGGER = logging.getLogger(__name__)
 
+
+def build_settings(accel, latpath, start=None, end=None):
+    """Convenience method for initialize SettingsFactory and generate settings.
+
+       :param accel: accelerator design description element
+       :param latpath: path to lattice file (test.in)
+       :param start: name of accelerator element to start processing
+       :param end: name of accelerator element to end processing
+    """
     settings_factory = SettingsFactory(accel, latpath)
 
     if start != None:
@@ -28,6 +37,12 @@ def build_settings(accel, latpath, start=None, end=None):
 
 
 class SettingsFactory(object):
+    """SettingsFactory is a factory class to build a settings dictionary from
+       and IMPACT lattice file (test.in) based on an Accelerator Design Description
+
+       :param accel: accelerator design description element
+       :param latpath: path to lattice file (test.in)
+    """
 
     def __init__(self, accel, latpath):
         self.accel = accel
@@ -82,7 +97,8 @@ class SettingsFactory(object):
 
 
     def build(self):
-
+        """Generate the settings dictionary from the IMPACT lattice file.
+        """
         if not os.path.isfile(self._latpath):
             raise RuntimeError("SettingsFactory: IMPACT lattice file not found: {}".format(self._latpath))
 
@@ -97,10 +113,12 @@ class SettingsFactory(object):
                 (latidx, latelm) = next(lattice_element)
 
                 if latelm[3] not in [ "103", "110" ]:
-                    raise Exception("SettingsFactory: expecting cavity element at line {}, found element: {}".format(latidx+1, latelm))
+                    raise RuntimeError("SettingsFactory: {} at line {}: unexpected element found: {}".format(elem.name, latidx+1, latelm))
 
-                if float(latelm[0]) != elem.length:
-                    raise Exception("SettingsFactory: expecting cavity element at line {} with length {}: expecting length {}".format(latidx+1, latelm[0], elem.length))
+                if not self._isclose(float(latelm[0]), elem.length):
+                    raise RuntimeError("SettingsFactory: {} at line {}: unexpected element length: {} ({})".format(elem.name, latidx+1, latelm[0], elem.length))
+
+                _LOGGER.info("SettingsFactory: %s at line %s: %s", elem.name, latidx+1, latelm)
 
                 settings[elem.channels.phase_cset] = OrderedDict([ ("VAL", float(latelm[6])) ])
                 settings[elem.channels.phase_rset] = OrderedDict(settings[elem.channels.phase_cset])
@@ -110,65 +128,158 @@ class SettingsFactory(object):
                 settings[elem.channels.amplitude_rset] = OrderedDict(settings[elem.channels.amplitude_cset])
                 settings[elem.channels.amplitude_read] = OrderedDict(settings[elem.channels.amplitude_cset])
 
+
             elif isinstance(elem, SolCorrElement):
                 # Solenoid elements are normally split into a number of steps, add these steps to get the total length.
+                hkick = None
+                vkick = None
                 length = 0.0
                 while length < elem.length:
                     (latidx, latelm) = next(lattice_element)
-                    if latelm[3] != "3":
-                        raise Exception("SettingsFactory: expecting solenoid element at line {}, found element: {}".format(latidx+1, latelm))
-                    length += float(latelm[0])
 
-                if length > elem.length:
-                    raise Exception("SettingsFactory: expecting solenoid element at line {} with length {}: expecting length {}".format(latidx+1, latelm[0], elem.length))
+                    if latelm[3] == "3":
+                         length += float(latelm[0])
+
+                    elif latelm[3] == "-21":
+                        hkick = float(latelm[6])
+                        vkick = float(latelm[8])
+
+                    else:
+                        raise RuntimeError("SettingsFactory: {} at line {}: unexpected element found: {}".format(elem.name, latidx+1, latelm))
+
+                    _LOGGER.info("SettingsFactory: %s at line %s: %s", elem.name, latidx+1, latelm)
+
+                if not self._isclose(length, elem.length):
+                    raise RuntimeError("SettingsFactory: {} at line {}: unexpected element length: {} ({})".format(elem.name, latidx+1, latelm[0], elem.length))
 
                 settings[elem.channels.field_cset] = OrderedDict([ ("VAL", float(latelm[4])) ])
                 settings[elem.channels.field_rset] = OrderedDict(settings[elem.channels.field_cset])
                 settings[elem.channels.field_read] = OrderedDict(settings[elem.channels.field_cset])
-                settings[elem.channels.hkick_cset] = OrderedDict([ ("VAL", 0.0) ])
+
+                if hkick != None:
+                    settings[elem.channels.hkick_cset] = OrderedDict([ ("VAL", hkick ) ])
+                else:     
+                    _LOGGER.warning("SettingsFactory: %s: Missing horizontal kick setting, assuming 0.0", elem.name)
+                    settings[elem.channels.hkick_cset] = OrderedDict([ ("VAL", 0.0 ) ])
                 settings[elem.channels.hkick_rset] = OrderedDict(settings[elem.channels.hkick_cset])
                 settings[elem.channels.hkick_read] = OrderedDict(settings[elem.channels.hkick_cset])
-                settings[elem.channels.vkick_cset] = OrderedDict([ ("VAL", 0.0) ])
-                settings[elem.channels.vkick_rset] = OrderedDict(settings[elem.channels.hkick_cset])
-                settings[elem.channels.vkick_read] = OrderedDict(settings[elem.channels.hkick_cset])
+
+                if vkick != None:
+                    settings[elem.channels.vkick_cset] = OrderedDict([ ("VAL", vkick) ])
+                else:
+                    _LOGGER.warning("SettingsFactory: %s: Missing vertical kick setting, assuming 0.0", elem.name)
+                    settings[elem.channels.vkick_cset] = OrderedDict([ ("VAL", 0.0) ])
+                settings[elem.channels.vkick_rset] = OrderedDict(settings[elem.channels.vkick_cset])
+                settings[elem.channels.vkick_read] = OrderedDict(settings[elem.channels.vkick_cset])
+
+
+            elif isinstance(elem, CorrElement):
+                (latidx, latelm) = next(lattice_element)
+
+                if latelm[3] not in [ "-21" ]:
+                    raise RuntimeError("SettingsFactory: {} at line {}: unexpected element found: {}".format(elem.name, latidx+1, latelm))
+
+                _LOGGER.info("SettingsFactory: %s at line %s: %s", elem.name, latidx+1, latelm)
+
+                # Do not check the element length. Element -21 typically has length 0.0 with drift before and after the element.
+                #if not self._isclose(float(latelm[0]), elem.length):
+                #    raise RuntimeError("SettingsFactory: {} at line {}: unexpected element length: {} ({})".format(elem.name, latidx+1, latelm[0], elem.length))
+
+                settings[elem.channels.hkick_cset] = OrderedDict([ ("VAL", float(latelm[6])) ])
+                settings[elem.channels.hkick_rset] = OrderedDict(settings[elem.channels.hkick_cset])
+                settings[elem.channels.hkick_read] = OrderedDict(settings[elem.channels.hkick_cset])
+                settings[elem.channels.vkick_cset] = OrderedDict([ ("VAL", float(latelm[8])) ])
+                settings[elem.channels.vkick_rset] = OrderedDict(settings[elem.channels.vkick_cset])
+                settings[elem.channels.vkick_read] = OrderedDict(settings[elem.channels.vkick_cset])
+
+
+            elif isinstance(elem, BendElement):
+                length = 0.0
+                while length < elem.length:
+                    (latidx, latelm) = next(lattice_element)
+                    
+                    if latelm[3] in [ "4" ]:
+                        length += float(latelm[0])
+                    else:
+                        raise RuntimeError("SettingsFactory: {} at line {}: unexpected element found: {}".format(elem.name, latidx+1, latelm))
+
+                    _LOGGER.info("SettingsFactory: %s at line %s: %s", elem.name, latidx+1, latelm)
+                   
+                if not self._isclose(length, elem.length):
+                    raise RuntimeError("SettingsFactory: {} at line {}: unexpected element length: {} ({})".format(elem.name, latidx+1, length, elem.length))
+
+                settings[elem.channels.angle_cset] = OrderedDict([ ("VAL", float(latelm[4])) ])
+                settings[elem.channels.angle_rset] = OrderedDict(settings[elem.channels.angle_cset])
+                settings[elem.channels.angle_read] = OrderedDict(settings[elem.channels.angle_cset])
+
 
             elif isinstance(elem, QuadElement):
                 (latidx, latelm) = next(lattice_element)
 
-                if latelm[3] != "1":
-                    raise Exception("SettingsFactory: expecting QuadElement at line {}: found element: {}".format(latidx+1, latelm))
-        
-                if float(latelm[0]) != elem.length:
-                    raise Exception("SettingsFactory: expecting QuadElement at line {} with length {}: expecting length {}".format(latidx+1, latelm[0], elem.length))
+                if latelm[3] not in [ "1", "5" ]:
+                    raise Exception("SettingsFactory: {} at line {}: unexpected element found: {}".format(elem.name, latidx+1, latelm))
 
+                _LOGGER.info("SettingsFactory: %s at line %s: %s", elem.name, latidx+1, latelm)
+        
+                if not self._isclose(float(latelm[0]), elem.length):
+                    raise Exception("SettingsFactory: {} at line {}: unexpected element length: {} ({})".format(elem.name, latidx+1, latelm[0], elem.length))
+
+                # Both element type 1 and 5 have the quad field strength at index 4. No special logic required!
                 settings[elem.channels.gradient_cset] = OrderedDict([ ("VAL", float(latelm[4])) ])
                 settings[elem.channels.gradient_rset] = OrderedDict(settings[elem.channels.gradient_cset])
                 settings[elem.channels.gradient_read] = OrderedDict(settings[elem.channels.gradient_cset])
 
-            elif isinstance(elem, CorrElement):
-                settings[elem.channels.hkick_cset] = OrderedDict([ ("VAL", 0.0) ])
-                settings[elem.channels.hkick_rset] = OrderedDict(settings[elem.channels.hkick_cset])
-                settings[elem.channels.hkick_read] = OrderedDict(settings[elem.channels.hkick_cset])
-                settings[elem.channels.vkick_cset] = OrderedDict([ ("VAL", 0.0) ])
-                settings[elem.channels.vkick_rset] = OrderedDict(settings[elem.channels.vkick_cset])
-                settings[elem.channels.vkick_read] = OrderedDict(settings[elem.channels.vkick_cset])
 
-            #elif isinstance(elem, cs.CSElement):
-            #    settings[elem.name] = {}
+            elif isinstance(elem, HexElement):
+                #(latidx, latelm) = next(lattice_element)
+                #
+                #if latelm[3] not in [ "5" ]:
+                #    raise Exception("SettingsFactory: {} at line {}: unexpected element found: {}".format(elem.name, latidx+1, latelm))
+                #
+                #_LOGGER.info("SettingsFactory: %s at line %s: %s", elem.name, latidx+1, latelm)
+                #
+                #if not self._isclose(float(latelm[0]), elem.length):
+                #    raise Exception("SettingsFactory: {} at line {}: unexpected element length: {} ({})".format(elem.name, latidx+1, latelm[0], elem.length))
+                #
+                _LOGGER.warning("SettingsFactory: Hexapole magnet element support not implemented. Assume field setting 0.0 T/m^2.")
 
-            #elif isinstance(elem, mag.HexElement):
-            #    pass
+                settings[elem.channels.field_cset] = OrderedDict([ ("VAL", 0.0) ])
+                settings[elem.channels.field_rset] = OrderedDict(settings[elem.channels.field_cset])
+                settings[elem.channels.field_read] = OrderedDict(settings[elem.channels.field_cset])
+
+
+            elif isinstance(elem, ChgStripElement):
+                (latidx, latelm) = next(lattice_element)
+
+                if latelm[3] != "-11":
+                    raise Exception("SettingsFactory: {} at line {}: unexpecting element found: {}".format(elem.name, latidx+1, latelm))
+
+                _LOGGER.info("SettingsFactory: %s at line %s: %s", elem.name, latidx+1, latelm)
+        
+                # Do not check element length. Element -11 typically has length 0.0, with drift before and after element.
+                #if not self._isclose(float(latelm[0]), elem.length):
+                #    raise Exception("SettingsFactory: {} at line {}: unexpecting element length: {} ({})".format(elem.name, latidx+1, latelm[0], elem.length))
+
 
             elif isinstance(elem, (BLMElement, BCMElement, BPMElement, BLElement, PMElement)):
                 pass # ignore diagnostic elements
 
+
             elif isinstance(elem, (DriftElement, ValveElement, PortElement)):
                 pass # ignore passive elements
+
             
             else:
                 raise Exception("SettingsFactory: unsupported accel element: {}".format(elem))
 
         return settings
+
+
+    @staticmethod
+    def _isclose(a, b, rtol=1e-5, atol=1e-8):
+        # Implementation adopted from NumPy 'isclose()'
+        # including the default values for rtol and atol.
+        return (abs(a - b) <= (atol + rtol*abs(b)))
 
 
 class _LatticeIterator():
@@ -197,7 +308,8 @@ class _LatticeIterator():
             if line.startswith("!"):
                 continue
             elm = line.strip().split()
-            if (len(elm) <= 3) or (float(elm[3]) <= 0):
+            if float(elm[3]) in [ 0, -13, -28 ]:
+                _LOGGER.debug("SettingsFactory: Skipping line %s: %s", self._idx+1, elm)
                 continue
             return (self._idx, elm)
 
