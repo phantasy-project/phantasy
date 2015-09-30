5c1a7571fbda702ddac09235b8d2e3d3d0e42f47
Dylan Maxwell <maxwelld@frib.msu.edu>
2015-09-30 16:12:19 -0400

    Update setup script to include extra for Lattice Model Service (LMS).

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@
 
 MAJOR = 0
 MINOR = 2
-PATCH = 0
+MICRO = 1
 ISRELEASED = True
-VERSION = '%d.%d.%d' % (MAJOR, MINOR, PATCH)
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 from setuptools import setup, find_packages
 setup(
@@ -29,19 +29,23 @@ setup(
     install_requires = [
         # PyPI
         'numpy>=1.8',
-        'scipy>=0.14',
+        #'scipy>=0.14',
         'xlrd>=0.9',
-        'tornado>=4.2',
-        'jinja2>=2.7',
-        'motor>=0.4',
         'functools32>=3.2',
-        'jsonschema>=2.5',
-        'matplotlib>=1.4.2',
+        #'matplotlib>=1.4.2',
         # Custom
-        'channelfinder>=2.0',
-        'PyScanClient>=0.9.5',
-        'cothread>=2.13'
+        #'channelfinder>=2.0',
+        #'PyScanClient>=0.9.5',
+        #'cothread>=2.13'
     ],
+    extras_requires = {
+        "LMS":[
+            'jsonschema>=2.5',
+            'tornado>=4.2',
+            'jinja2>=2.7',
+            'motor>=0.4'
+        ]
+    },
     dependency_links=[
         'git+https://github.com/ChannelFinder/pyCFClient.git',
         'git+https://github.com/PythonScanClient/PyScanClient.git',
