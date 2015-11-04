3d5f7d807558e4fe7db2b3c4d829ff3a510e3e96
Dylan Maxwell <maxwelld@frib.msu.edu>
2015-11-04 17:25:25 -0500

    Move the search 'compare' button to fixed position on left side.

--- phyutil/phyapp/latticemodel/service/static/latticemodel/js/model_search.js
+++ phyutil/phyapp/latticemodel/service/static/latticemodel/js/model_search.js
@@ -3,26 +3,49 @@
  *
  */
 
-$('#model_name').typeahead({hint:false}, {
-	source:function(query, syncResults, asyncResults) {
-		latticemodel.data.find_model_names(query, asyncResults);
-	},
-	limit:10
-});
+(function() {
 
-$('.compare-checkbox').on("change", function() {
-	var checked = $('.compare-checkbox:checked').length;
-	if( checked < 2) {
+	function hideCompareButton() {
 		$('.compare-button').addClass('hidden');
-	} else if( checked == 2 ) {
-		$('.compare-button').removeClass('hidden');
-	} else if( checked == 3) {
-		$(this).prop("checked", false);
-		alert("Only two Lattice can be compared.");
-	}
-});
+	};
 
-$('.compare-button').on('click', function() {
-	$('.compare-form').submit();
-});
+	function showCompareButton() {
+		$('.compare-button').removeClass('hidden');
+	};
+
+	function toggleCompareButton() {
+		var checked = $('.compare-checkbox:checked').length;
+		if( checked < 2) {
+			hideCompareButton();
+		} else if( checked == 2 ) {
+			showCompareButton();
+		} else if( checked == 3) {
+			$(this).prop("checked", false);
+			alert("Only two Lattice can be compared.");
+		}
+	};
+
+	$('#model_name').typeahead({hint:false}, {
+		source:function(query, syncResults, asyncResults) {
+			latticemodel.data.find_model_names(query, asyncResults);
+		},
+		limit:10
+	});
+
+	$('.compare-checkbox').on("change", toggleCompareButton);
+
+	$('a[href="#results"]')
+		.on('show.bs.tab', toggleCompareButton)
+		.on('hide.bs.tab', hideCompareButton);
+
+	$('.compare-button').on('click', function() {
+		$('.compare-form').submit();
+	});
+
+	// Initialize compare button because of possible autocomplete.
+	toggleCompareButton();
+
+})();
+
+
 
