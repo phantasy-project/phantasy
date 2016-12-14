8c0a76492888397596eb38afdeaba73c646ba8f4
Tong Zhang <zhangt@frib.msu.edu>
2016-12-14 11:21:52 -0500

    add new functions into miscutils

--- phyapps/utils/miscutils.py
+++ phyapps/utils/miscutils.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-""" other support utils
-
-.. moduleauthor:: Tong Zhang <zhangt@frib.msu.edu>
-
-:date: 2016-11-22 11:21:15 AM EST
+"""Other support utils.
 """
 
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import print_function
+
 import logging
+from bisect import bisect
+from fnmatch import fnmatch
+
 from flame import Machine
 
 
+__authors__ = "Tong Zhang"
+__copyright__ = "(c) 2016, Facility for Rare Isotope beams, Michigan State University"
+__contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
+
+
 _LOGGER = logging.getLogger(__name__)
+
 
 def _flatten(nnn):
     """ flatten recursively defined list or tuple
@@ -34,6 +44,7 @@ def _flatten(nnn):
         else:
             yield(nn)
 
+
 def flatten(nnn):
     """ flatten recursively defined list or tuple
 
@@ -48,6 +59,7 @@ def flatten(nnn):
     [1,2,3,4,5,6,7,8,9,10,'x','y']
     """
     return list(_flatten(nnn))
+
 
 def get_intersection(**kws):
     """Get the intersection of all input keyword parameters, ignore
@@ -84,6 +96,7 @@ def get_intersection(**kws):
             s = s.intersection(v)
     return list(s)
     
+
 def machine_setter(_latfile=None, _machine=None, _handle_name=None):
     """ set flame machine, prefer *_latfile*
 
@@ -105,3 +118,63 @@ def machine_setter(_latfile=None, _machine=None, _handle_name=None):
         else:
             m = _machine
     return m
+
+
+def bisect_index(x, val):
+    """Get the *val* index in sorted *x*.
+
+    Parameters
+    ----------
+    x : List, sequence
+        Sorted sequence.
+    val : 
+        Value to be located.
+
+    Returns
+    -------
+    i : int
+        Index of *val* that should be inserted into *x*.
+
+    Examples
+    --------
+    >>> x = [0.2070635,0.51132684,0.6433303399999999,
+             0.74333034,0.74333034,0.9867243400000001,
+             1.7663703400000001]
+    >>> bisect_index(x, 1.5)
+    6
+    >>> bisect_index(x, 0.5)
+    1
+    >>> x[1:6]
+    [0.51132684, 0.6433303399999999, 0.74333034, 0.74333034,
+     0.9867243400000001]
+    """
+    return bisect(x, val)
+
+
+def pattern_filter(x, pattern):
+    """Get sub sequence from sequence by applying filter.
+    
+    Parameters
+    ----------
+    x : sequence
+        List or tuple to be filtered.
+    pattern : str
+        Unix shell style pattern to be as filter.
+
+    Returns
+    -------
+    ret : List
+        List filtered out.
+
+    Examples
+    --------
+    >>> a = ['BPM', 'BEND', 'VCOR', 'PM', 'HCOR']
+    >>> pattern_filter(a, 'BP*')
+    ['BPM']
+    >>> pattern_filter(a, '*PM*')
+    ['BPM', 'PM']
+    >>> pattern_filter(a, '*COR')
+    ['VCOR', 'HCOR']
+    """
+    return [i for i in x if fnmatch(i, pattern)]
+
