1d1cc65ce6d306f548e725718c8c02d2fec5daac
Tong Zhang <zhangt@frib.msu.edu>
2017-03-16 13:16:36 -0400

    Fix - wrong version.
    - should be 0.2.1.

--- phantasy/__init__.py
+++ phantasy/__init__.py
@@ -24,7 +24,7 @@ from phantasy.library import *
 __authors__ = "Tong Zhang"
 __copyright__ = "(c) 2016, Facility for Rare Isotope beams, Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 __doc__ = """PHANTASY:
 Physics high-level applications and toolkits for accelerator system.
