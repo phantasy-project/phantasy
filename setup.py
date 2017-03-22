3f2102b3742ea3e337c40964e0a83e2cd76c3c9e
Tong Zhang <zhangt@frib.msu.edu>
2017-03-22 10:31:30 -0400

    Version: 0.2.2.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ app_scripts = [i for i in glob.glob("scripts/*") if not fnmatch(i, "scripts/soft
 
 setup(
         name=app_name,
-        version="0.2.1",
+        version="0.2.2",
         description=app_description,
         long_description=app_long_description,
         author=app_author,
