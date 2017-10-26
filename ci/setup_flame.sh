aa3ed1fde7f67fedf153595927240da400c24508
Tong Zhang <zhangt@frib.msu.edu>
2017-10-26 11:34:56 -0400

    CI - Update policy for installing flame packages.

--- ci/setup_flame.sh
+++ ci/setup_flame.sh
@@ -4,11 +4,12 @@
 # install
 # 
 
+VERSION=1.2.0
 cwd=$(pwd)
 cd $HOME
 git clone -b latest https://github.com/archman/FLAME.git
 cd FLAME/deb/trusty
-sudo dpkg -i *.deb
+sudo dpkg -i *${VERSION}*.deb
 cd ${cwd}
 
 #export PYTHONPATH=/usr/lib/python2.7/dist-packages/:${PYTHONPATH}
