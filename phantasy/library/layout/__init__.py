ad1760bfd8a8a4dd18bc67c093063bb9f638f310
Tong Zhang <zhangt@frib.msu.edu>
2018-03-12 22:28:21 -0400

    Add new elements into init file.

--- phantasy/library/layout/__init__.py
+++ phantasy/library/layout/__init__.py
@@ -1,14 +1,37 @@
-from .layout import build_layout, Layout
-from .accel import BCMElement, BLElement, BLMElement, BPMElement
-from .accel import BendElement, CavityElement, ColumnElement
-from .accel import CorElement, DriftElement, EBendElement
-from .accel import EMSElement, EQuadElement, ElectrodeElement
-from .accel import FCElement, HCorElement, PMElement
-from .accel import PortElement, QuadElement, SeqElement
-from .accel import SextElement, SolCorElement, SolElement
-from .accel import StripElement, VCorElement, VDElement
+from .layout import build_layout
+from .layout import Layout
+from .accel import BCMElement
+from .accel import BLElement
+from .accel import BLMElement
+from .accel import BPMElement
+from .accel import BendElement
+from .accel import CavityElement
+from .accel import ColumnElement
+from .accel import CorElement
+from .accel import DriftElement
+from .accel import EBendElement
+from .accel import EMSElement
+from .accel import EQuadElement
+from .accel import ElectrodeElement
+from .accel import FCElement
+from .accel import HCorElement
+from .accel import PMElement
+from .accel import PortElement
+from .accel import QuadElement
+from .accel import SeqElement
+from .accel import SextElement
+from .accel import SolCorElement
+from .accel import SolElement
+from .accel import StripElement
+from .accel import VCorElement
+from .accel import VDElement
+from .accel import SlitElement
+from .accel import ChopperElement
+from .accel import AttenuatorElement
+from .accel import DumpElement
 from .accel import ValveElement
-from .accel import Element, Fields
+from .accel import Element
+from .accel import Fields
 
 __all__ = [
     'build_layout', 'Layout',
@@ -20,6 +43,7 @@ __all__ = [
     'PortElement', 'QuadElement', 'SeqElement',
     'SextElement', 'SolCorElement', 'SolElement',
     'StripElement', 'VCorElement', 'VDElement', 'ValveElement',
+    'SlitElement', 'ChopperElement', 'AttenuatorElement', 'DumpElement',
     'Element', 'Fields',
 ]
 
