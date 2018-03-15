673b40cb01d726590c2295da5524890ac068392d
Tong Zhang <zhangt@frib.msu.edu>
2018-03-15 17:01:13 -0400

    Add module to disable logging message wrt level.

--- phantasy/library/misc/__init__.py
+++ phantasy/library/misc/__init__.py
@@ -10,13 +10,13 @@ from .miscutils import simplify_data
 from .miscutils import complicate_data
 from .miscutils import SpecialDict
 from .miscutils import parse_dt
-
+from .message import disable_warnings
 from .httputils import cofetch
 
 __all__ = [
     'flatten', 'get_intersection', 'machine_setter', 'bisect_index',
     'pattern_filter', 'expand_list_to_dict', 'simplify_data',
     'complicate_data', 'SpecialDict', 'parse_dt',
-    'cofetch',
+    'cofetch', 'disable_warnings',
 ]
 
---
+++ phantasy/library/misc/message.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+
+def disable_warnings(level=60):
+    logging.disable(level)
