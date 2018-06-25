4777e842af411c5900725389db3d1879bfb9a5f8
Tong Zhang <zhangt@frib.msu.edu>
2018-06-25 16:14:53 -0400

    ENH: Make scan module optional.

--- phantasy/library/scan/__init__.py
+++ phantasy/library/scan/__init__.py
@@ -1,5 +1,19 @@
-from .baseclient import BaseScanClient
-from .client1 import ScanClient1D
-from .datautil import ScanDataFactory
+import logging
 
-__all__ = ['BaseScanClient', 'ScanClient1D', 'ScanDataFactory']
+_LOGGER = logging.getLogger(__name__)
+
+
+try:
+    import scan
+    HAS_SCAN = True
+except ImportError:
+    HAS_SCAN = False
+    _LOGGER.warn("Package 'scanclient' does not exit, some" \
+            " features may not be available.")
+
+if HAS_SCAN:
+    from .baseclient import BaseScanClient
+    from .client1 import ScanClient1D
+    from .datautil import ScanDataFactory
+
+    __all__ = ['BaseScanClient', 'ScanClient1D', 'ScanDataFactory']
