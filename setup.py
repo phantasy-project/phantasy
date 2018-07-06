a97068c39572c1fdf41bac73b4bc3bf193e9badd
Tong Zhang <zhangt@frib.msu.edu>
2018-07-06 17:34:10 -0400

    MNT: Bump up version to 0.9.7.

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ d_pkg.update(
 
 setup(
     name=app_name,
-    version="0.9.6",
+    version="0.9.7",
     description=app_description,
     long_description=app_long_description,
     author=app_author,
