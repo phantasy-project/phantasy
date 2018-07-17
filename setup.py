7698f8a44b9beaf1311e642d622ecdabe642ca59
Tong Zhang <zhangt@frib.msu.edu>
2018-07-17 09:01:20 -0400

    MNT: Bump up version to 0.9.8.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read_license():
         return f.read()
 
 
-app_version="0.9.7"
+app_version="0.9.8"
 app_name = "phantasy"
 app_description = 'Physics high-level applications and toolkit for ' \
         'accelerator system'
