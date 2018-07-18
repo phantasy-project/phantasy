15b633ec6dbc2d7528c5d179b0146b2799728cb5
Tong Zhang <zhangt@frib.msu.edu>
2018-07-18 11:10:40 -0400

    ENH: Fix tests directory and suppress output.

--- phantasy/tests/__init__.py
+++ phantasy/tests/__init__.py
@@ -1,19 +1,22 @@
+# -*- coding: utf-8 -*-
+
 import nose
+import os
 
 try:
     basestring
 except NameError:
     basestring = str
 
+CWD = os.path.dirname(__file__)
 
-def run(argv=None):
-    argv_list = ['--logging-clear-handlers', '--nologcapture']
+
+def run(argv=None, **kws):
+    argv_list = ['--logging-clear-handlers', '--logging-level=INFO']
     if argv is not None:
         if isinstance(argv, basestring):
             argv = argv,
         argv_list.extend(argv)
-        nose.run(argv=list(set(argv_list)))
+        nose.run(argv=list(set(argv_list)), defaultTest=CWD, **kws)
     else:
-        nose.run(argv=argv_list)
-
-             
+        nose.run(argv=argv_list, defaultTest=CWD, **kws)
