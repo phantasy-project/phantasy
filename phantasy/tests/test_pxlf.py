6c15eea5ef689077bed461375f8a62f7fc22646d
Tong Zhang <zhangt@frib.msu.edu>
2018-07-18 11:19:04 -0400

    FIX: Write permission.

--- phantasy/tests/test_pxlf.py
+++ phantasy/tests/test_pxlf.py
@@ -15,12 +15,13 @@ XLF_PATH = os.path.join(MACH_PATH, 'T30102-CM-000155-R001_20170724.xlsx')
 CONFIG_PATH = os.path.join(MACH_PATH, 'phantasy.cfg')
 LAYOUT_PATH = os.path.join(MACH_PATH, 'layout_bak.csv')
 CHANNELS_PATH = os.path.join(MACH_PATH, 'channels_bak.csv')
+TEST_PATH = "/tmp"
 
 
 class TestXLFParser(unittest.TestCase):
     def setUp(self):
-        self.layout_file = os.path.join(MACH_PATH, 'layout.csv')
-        self.channels_file = os.path.join(MACH_PATH, 'channels.csv')
+        self.layout_file = os.path.join(TEST_PATH, 'layout.csv')
+        self.channels_file = os.path.join(TEST_PATH, 'channels.csv')
         self.start_elem = 'FE_ISRC1:ACC_DFT_D0700_1'
         self.end_elem = 'FE_LEBT:GV_DFT_D0999_3'
 
