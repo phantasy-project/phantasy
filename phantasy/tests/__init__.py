fbfd4f83e502d9d3a3d23278bc6dde60f24cbc24
Tong Zhang <zhangt@frib.msu.edu>
2018-07-18 11:59:55 -0400

    ENH: Clean up after running tests.

--- phantasy/tests/__init__.py
+++ phantasy/tests/__init__.py
@@ -2,6 +2,7 @@
 
 import nose
 import os
+import shutil
 
 try:
     basestring
@@ -9,10 +10,12 @@ except NameError:
     basestring = str
 
 CWD = os.path.dirname(__file__)
-
+TMP_DIR = "/tmp/phantasy_data"
 
 def run(argv=None, **kws):
-    argv_list = ['--logging-clear-handlers', '--logging-level=INFO']
+    argv_list = ['--logging-clear-handlers',
+                 '--logging-level=INFO',
+                 '--nologcapture']
     if argv is not None:
         if isinstance(argv, basestring):
             argv = argv,
@@ -20,3 +23,5 @@ def run(argv=None, **kws):
         nose.run(argv=list(set(argv_list)), defaultTest=CWD, **kws)
     else:
         nose.run(argv=argv_list, defaultTest=CWD, **kws)
+    if os.path.isdir(TMP_DIR):
+        shutil.rmtree(TMP_DIR)
