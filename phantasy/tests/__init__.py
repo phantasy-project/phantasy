63df1683154ff1df2052a72a2e8f3df4979d3615
Tong Zhang <zhangt@frib.msu.edu>
2018-07-18 15:09:18 -0400

    ENH: Make 'test_phantasy' command accept options.

--- phantasy/tests/__init__.py
+++ phantasy/tests/__init__.py
@@ -3,6 +3,7 @@
 import nose
 import os
 import shutil
+import sys
 
 try:
     basestring
@@ -12,10 +13,19 @@ except NameError:
 CWD = os.path.dirname(__file__)
 TMP_DIR = "/tmp/phantasy_data"
 
-def run(argv=None, **kws):
+def main(argv=None, **kws):
+    """Run tests for `phantasy` package, accept CLI options when executing
+    `run_phantasy` command.
+
+    Parameters
+    ----------
+    argv : list
+        List of options of `nosetests`.
+    """
     argv_list = ['--logging-clear-handlers',
                  '--logging-level=INFO',
                  '--nologcapture']
+    argv_list.extend(sys.argv[1:])
     if argv is not None:
         if isinstance(argv, basestring):
             argv = argv,
@@ -23,6 +33,7 @@ def run(argv=None, **kws):
         nose.run(argv=list(set(argv_list)), defaultTest=CWD, **kws)
     else:
         nose.run(argv=argv_list, defaultTest=CWD, **kws)
+
     if os.path.isdir(TMP_DIR):
         shutil.rmtree(TMP_DIR)
 
