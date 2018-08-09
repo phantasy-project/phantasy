91bf2399c47d0559a3003a2ec05c830ca22b9dde
Tong Zhang <zhangt@frib.msu.edu>
2018-08-09 15:56:54 -0400

    ENH: Add new function to copy default SQLite file.

--- phantasy/apps/unicorn/utils.py
+++ phantasy/apps/unicorn/utils.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import os
+import shutil
 
 
 def get_service_status(url):
@@ -9,6 +11,20 @@ def get_service_status(url):
         return "Running"
     except requests.ConnectionError:
         return "Not Running"
+
+def init_unicorn_database():
+    """Copy default one into user's own directory.
+    """
+    default_dir_name = os.path.expanduser('~/.unicorn')
+    default_file_name = 'unicorn.sqlite'
+    full_name = os.path.join(default_dir_name, default_file_name)
+    db_src = os.path.join('/usr/share/unicorn/unicorn.sqlite')
+    if os.path.exists(default_dir_name):
+        if not os.path.exists(full_name):
+            shutil.copy(db_src, default_dir_name)
+    else:
+        os.mkdir(default_dir_name)
+        shutil.copy(db_src, default_dir_name)
 
 
 if __name__ == "__main__":
