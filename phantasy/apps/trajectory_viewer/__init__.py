786efca5636cb702a1f4f3515b4ab443eb7e4ec0
Tong Zhang <zhangt@frib.msu.edu>
2018-09-26 15:51:02 -0400

    MNT: Bump up Trajectory Viewer app version to 1.1.

--- phantasy/apps/trajectory_viewer/__init__.py
+++ phantasy/apps/trajectory_viewer/__init__.py
@@ -9,11 +9,14 @@ __authors__ = "Tong Zhang"
 __copyright__ = "(c) 2018, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
-__version__ = '1.0'
+__version__ = '1.1'
 
 
 def run(cli=False):
     app = QApplication(sys.argv)
+    #app.setStyle('Fusion')
+    #app.setStyle('Oxygen')
+    #app.setStyle('GTK+')
     w = TrajectoryViewerWindow(version=__version__)
     w.show()
     w.setWindowTitle("Trajectory Viewer")
