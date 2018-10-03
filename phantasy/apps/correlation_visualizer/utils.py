e242aeea95fe8b2b57d629a84cf84b23816764af
Tong Zhang <zhangt@frib.msu.edu>
2018-10-03 10:07:57 -0400

    ENH: Add 'milli_sleep' to do non-blocking wait.

--- phantasy/apps/correlation_visualizer/utils.py
+++ phantasy/apps/correlation_visualizer/utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import epics
+import time
 from PyQt5.QtCore import QTimer
 
 
@@ -15,6 +16,8 @@ class PVElement(object):
     >>> elem.value = 1 # put with a new value
     """
     def __init__(self, put_pv_name, get_pv_name):
+        self._put_pvname = put_pv_name
+        self._get_pvname = get_pv_name
         self._putPV = epics.PV(put_pv_name)
         self._getPV = epics.PV(get_pv_name)
 
@@ -35,6 +38,18 @@ class PVElement(object):
     def __repr__(self):
         return str(self._getPV), str(self._putPV)
 
+    @property
+    def get_pvname(self):
+        return self._get_pvname
+
+    @property
+    def put_pvname(self):
+        return self._put_pvname
+
+    @property
+    def pvname(self):
+        return self._put_pvname, self._get_pvname
+
 
 class PVElementReadonly(object):
     """Unified interface for `get` to a PV, i.e. readonly.
@@ -45,6 +60,7 @@ class PVElementReadonly(object):
     >>> elem.value # get value
     """
     def __init__(self, get_pv_name):
+        self._get_pvname = get_pv_name
         self._getPV = epics.PV(get_pv_name)
 
     @property
@@ -60,6 +76,13 @@ class PVElementReadonly(object):
     def __repr__(self):
         return str(self._getPV)
 
+    @property
+    def get_pvname(self):
+        return self._get_pvname
+
+    @property
+    def pvname(self):
+        return (self._get_pvname,)
 
 def delayed_exec(f, delay, *args, **kws):
     """Execute *f* after *delay* msecm `*args` and `**kws` is for *f*.
@@ -67,4 +90,10 @@ def delayed_exec(f, delay, *args, **kws):
     def func():
         return f(*args, **kws)
     QTimer.singleShot(delay, func)
+
+
+def milli_sleep(qApp, msec):
+    t0 = time.time()
+    while (time.time() - t0) * 1000 < msec:
+        qApp.processEvents()
 
