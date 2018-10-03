282ab30f92016e253d275f33fff0a1f2a71a42e9
Tong Zhang <zhangt@frib.msu.edu>
2018-10-03 17:05:34 -0400

    ENH: Add funtion to get file to save with QFileDialog.

--- phantasy/apps/correlation_visualizer/utils.py
+++ phantasy/apps/correlation_visualizer/utils.py
@@ -2,7 +2,9 @@
 
 import epics
 import time
+import re
 from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QFileDialog
 
 
 class PVElement(object):
@@ -84,6 +86,7 @@ class PVElementReadonly(object):
     def pvname(self):
         return (self._get_pvname,)
 
+
 def delayed_exec(f, delay, *args, **kws):
     """Execute *f* after *delay* msecm `*args` and `**kws` is for *f*.
     """
@@ -96,4 +99,35 @@ def milli_sleep(qApp, msec):
     t0 = time.time()
     while (time.time() - t0) * 1000 < msec:
         qApp.processEvents()
+
+
+def get_save_filename(obj, filter=None, caption=None):
+    """Get file for saving data.
+
+    Parameters
+    ----------
+    obj :
+        Parent widget of QFileDialog.
+    filter : str
+        File fileters, see `QFileDialog.setNameFilter`,
+        e.g. ``JSON Files (*.json);;HDF5 Files (*.hdf5 *.h5)``
+    caption : str
+        Caption of file dialog.
+
+    Returns
+    -------
+    r : str
+        Selected filename.
+    """
+    filepath, filter = QFileDialog.getSaveFileName(
+            obj, caption, "", filter)
+    if not filepath:
+        return None
+    ext = re.match(r'.*\.(.*)\)', filter).group(1)
+    if re.match(r'.*\..*', filepath) is not None:
+        filename = re.sub(r'([^.]*)(\.)(.*)', r'\1.{}'.format(ext), filepath)
+    else:
+        filename = re.sub(r'(.*)', r'\1.{}'.format(ext), filepath)
+
+    return filename
 
