a430cf6b0c5f0d4fbcf4586b7e9745015642d8b1
Tong Zhang <zhangt@frib.msu.edu>
2018-10-03 17:08:08 -0400

    ENH: Create new classes to manage scan out data.

--- phantasy/apps/correlation_visualizer/data.py
+++ phantasy/apps/correlation_visualizer/data.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import json
+from collections import OrderedDict
+from .data_templates import SHEET_TEMPLATES
+
+try:
+    basestring
+except NameError:
+    basestring = str
 
 
 class ScanDataModel(object):
@@ -50,3 +58,62 @@ class ScanDataModel(object):
 
     def get_yavg(self):
         return self._avg[:, 1]
+
+
+class DataSheetBase(OrderedDict):
+    def __init__(self, path=None):
+        super(DataSheetBase, self).__init__()
+        if isinstance(path, basestring):
+            with open(path, "r") as fp:
+                self.read(fp)
+
+    def read(self, fp):
+        """How to read the data sheet from file-like stream defined by *fp*.
+        """
+        raise NotImplementedError
+
+    def write(self, path=None):
+        """How to write the data sheet to file defined by *path*.
+        """
+        raise NotImplementedError
+
+    def __deepcopy__(self, memo):
+        s = DataSheetBase()
+        s.update(deepcopy(list(self.items()), memo))
+        return s
+
+    def load_template(self, template):
+        """Load template dict.
+
+        Parameters
+        ----------
+        template : str
+            Name of sheet template, all valid names could be returned from
+            :method:`~DataSheetBase.template_names()`.
+        """
+        temp_sheet_dict = SHEET_TEMPLATES[template]
+        self.update(temp_sheet_dict)
+
+    @staticmethod
+    def template_names():
+        """Return list of all valid sheet template names.
+        """
+        return list(SHEET_TEMPLATES.keys())
+
+
+class JSONDataSheet(DataSheetBase):
+    """Save/Open JSON formated data sheet.
+    """
+    def __init__(self, path=None):
+        super(JSONDataSheet, self).__init__(path)
+
+        # load 'Quad Scan' template
+        self.load_template('Quad Scan')
+
+    def read(self, fp):
+        self.update(json.load(fp, object_pairs_hook=OrderedDict))
+
+    def write(self, path):
+        with open(path, 'w') as fp:
+            json.dump(self, fp, indent=2, sort_keys=True)
+
