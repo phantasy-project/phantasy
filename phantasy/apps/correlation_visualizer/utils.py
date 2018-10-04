95559621d7ec82c335c6f7cf724a6ab7bb521fca
Tong Zhang <zhangt@frib.msu.edu>
2018-10-04 10:38:06 -0400

    MNT: Move get_save_filename to phantasy.apps.utils module.

--- phantasy/apps/correlation_visualizer/utils.py
+++ phantasy/apps/correlation_visualizer/utils.py
@@ -4,7 +4,6 @@ import epics
 import time
 import re
 from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QFileDialog
 
 
 class PVElement(object):
@@ -116,35 +115,3 @@ def milli_sleep(qApp, msec):
     t0 = time.time()
     while (time.time() - t0) * 1000 < msec:
         qApp.processEvents()
-
-
-def get_save_filename(obj, filter=None, caption=None):
-    """Get file for saving data.
-
-    Parameters
-    ----------
-    obj :
-        Parent widget of QFileDialog.
-    filter : str
-        File fileters, see `QFileDialog.setNameFilter`,
-        e.g. ``JSON Files (*.json);;HDF5 Files (*.hdf5 *.h5)``
-    caption : str
-        Caption of file dialog.
-
-    Returns
-    -------
-    r : str
-        Selected filename.
-    """
-    filepath, filter = QFileDialog.getSaveFileName(
-            obj, caption, "", filter)
-    if not filepath:
-        return None
-    ext = re.match(r'.*\.(.*)\)', filter).group(1)
-    if re.match(r'.*\..*', filepath) is not None:
-        filename = re.sub(r'([^.]*)(\.)(.*)', r'\1.{}'.format(ext), filepath)
-    else:
-        filename = re.sub(r'(.*)', r'\1.{}'.format(ext), filepath)
-
-    return filename
-
