a28d561c7c5592a36d7e81dbd8f4583afd38bb2c
Tong Zhang <zhangt@frib.msu.edu>
2018-10-11 15:16:13 -0400

    UI/CV: Update with new element selection module.

--- phantasy/apps/correlation_visualizer/ui/ui_app.py
+++ phantasy/apps/correlation_visualizer/ui/ui_app.py
@@ -58,69 +58,63 @@ class Ui_MainWindow(object):
         self.scan_groupBox.setObjectName("scan_groupBox")
         self.gridLayout = QtWidgets.QGridLayout(self.scan_groupBox)
         self.gridLayout.setObjectName("gridLayout")
-        self.scan_verticalLayout = QtWidgets.QVBoxLayout()
-        self.scan_verticalLayout.setObjectName("scan_verticalLayout")
         self.scan_gridLayout = QtWidgets.QGridLayout()
         self.scan_gridLayout.setObjectName("scan_gridLayout")
-        self.scan_vars_get_lineEdit = QtWidgets.QLineEdit(self.scan_groupBox)
-        self.scan_vars_get_lineEdit.setObjectName("scan_vars_get_lineEdit")
-        self.scan_gridLayout.addWidget(self.scan_vars_get_lineEdit, 1, 1, 1, 3)
-        self.scan_range_set_btn = QtWidgets.QPushButton(self.scan_groupBox)
-        self.scan_range_set_btn.setObjectName("scan_range_set_btn")
-        self.scan_gridLayout.addWidget(self.scan_range_set_btn, 3, 4, 1, 1)
-        self.label_8 = QtWidgets.QLabel(self.scan_groupBox)
-        self.label_8.setObjectName("label_8")
-        self.scan_gridLayout.addWidget(self.label_8, 0, 0, 1, 1)
+        self.select_alter_elem_btn = QtWidgets.QPushButton(self.scan_groupBox)
+        self.select_alter_elem_btn.setObjectName("select_alter_elem_btn")
+        self.scan_gridLayout.addWidget(self.select_alter_elem_btn, 1, 4, 1, 1)
         self.label_9 = QtWidgets.QLabel(self.scan_groupBox)
         self.label_9.setObjectName("label_9")
-        self.scan_gridLayout.addWidget(self.label_9, 3, 0, 1, 1)
-        self.label_10 = QtWidgets.QLabel(self.scan_groupBox)
-        self.label_10.setObjectName("label_10")
-        self.scan_gridLayout.addWidget(self.label_10, 3, 2, 1, 1)
-        self.lower_limit_lineEdit = QtWidgets.QLineEdit(self.scan_groupBox)
-        self.lower_limit_lineEdit.setPlaceholderText("")
-        self.lower_limit_lineEdit.setObjectName("lower_limit_lineEdit")
-        self.scan_gridLayout.addWidget(self.lower_limit_lineEdit, 3, 1, 1, 1)
+        self.scan_gridLayout.addWidget(self.label_9, 2, 0, 1, 1)
+        self.monitor_elem_lineEdit = QtWidgets.QLineEdit(self.scan_groupBox)
+        self.monitor_elem_lineEdit.setText("")
+        self.monitor_elem_lineEdit.setPlaceholderText("")
+        self.monitor_elem_lineEdit.setObjectName("monitor_elem_lineEdit")
+        self.scan_gridLayout.addWidget(self.monitor_elem_lineEdit, 3, 1, 1, 3)
         self.upper_limit_lineEdit = QtWidgets.QLineEdit(self.scan_groupBox)
         self.upper_limit_lineEdit.setPlaceholderText("")
         self.upper_limit_lineEdit.setObjectName("upper_limit_lineEdit")
-        self.scan_gridLayout.addWidget(self.upper_limit_lineEdit, 3, 3, 1, 1)
-        self.select_scan_elems_btn = QtWidgets.QPushButton(self.scan_groupBox)
-        self.select_scan_elems_btn.setObjectName("select_scan_elems_btn")
-        self.scan_gridLayout.addWidget(self.select_scan_elems_btn, 2, 3, 1, 1)
+        self.scan_gridLayout.addWidget(self.upper_limit_lineEdit, 2, 3, 1, 1)
         self.label_11 = QtWidgets.QLabel(self.scan_groupBox)
         self.label_11.setObjectName("label_11")
-        self.scan_gridLayout.addWidget(self.label_11, 4, 0, 1, 1)
-        self.label_13 = QtWidgets.QLabel(self.scan_groupBox)
-        self.label_13.setObjectName("label_13")
-        self.scan_gridLayout.addWidget(self.label_13, 1, 0, 1, 1)
-        self.scan_vars_type_cbb = QtWidgets.QComboBox(self.scan_groupBox)
-        self.scan_vars_type_cbb.setObjectName("scan_vars_type_cbb")
-        self.scan_vars_type_cbb.addItem("")
-        self.scan_vars_type_cbb.addItem("")
-        self.scan_gridLayout.addWidget(self.scan_vars_type_cbb, 2, 1, 1, 1)
-        self.scan_vars_put_lineEdit = QtWidgets.QLineEdit(self.scan_groupBox)
-        self.scan_vars_put_lineEdit.setPlaceholderText("")
-        self.scan_vars_put_lineEdit.setObjectName("scan_vars_put_lineEdit")
-        self.scan_gridLayout.addWidget(self.scan_vars_put_lineEdit, 0, 1, 1, 3)
-        self.monitor_vars_type_cbb = QtWidgets.QComboBox(self.scan_groupBox)
-        self.monitor_vars_type_cbb.setObjectName("monitor_vars_type_cbb")
-        self.monitor_vars_type_cbb.addItem("")
-        self.monitor_vars_type_cbb.addItem("")
-        self.scan_gridLayout.addWidget(self.monitor_vars_type_cbb, 5, 1, 1, 1)
-        self.select_monitor_elems_btn = QtWidgets.QPushButton(
+        self.scan_gridLayout.addWidget(self.label_11, 3, 0, 1, 1)
+        self.label_10 = QtWidgets.QLabel(self.scan_groupBox)
+        self.label_10.setObjectName("label_10")
+        self.scan_gridLayout.addWidget(self.label_10, 2, 2, 1, 1)
+        self.lower_limit_lineEdit = QtWidgets.QLineEdit(self.scan_groupBox)
+        self.lower_limit_lineEdit.setPlaceholderText("")
+        self.lower_limit_lineEdit.setObjectName("lower_limit_lineEdit")
+        self.scan_gridLayout.addWidget(self.lower_limit_lineEdit, 2, 1, 1, 1)
+        self.alter_elem_lineEdit = QtWidgets.QLineEdit(self.scan_groupBox)
+        self.alter_elem_lineEdit.setText("")
+        self.alter_elem_lineEdit.setPlaceholderText("")
+        self.alter_elem_lineEdit.setObjectName("alter_elem_lineEdit")
+        self.scan_gridLayout.addWidget(self.alter_elem_lineEdit, 1, 1, 1, 3)
+        self.label_8 = QtWidgets.QLabel(self.scan_groupBox)
+        self.label_8.setObjectName("label_8")
+        self.scan_gridLayout.addWidget(self.label_8, 1, 0, 1, 1)
+        self.select_more_monitor_elems_btn = QtWidgets.QPushButton(
             self.scan_groupBox)
-        self.select_monitor_elems_btn.setObjectName("select_monitor_elems_btn")
-        self.scan_gridLayout.addWidget(self.select_monitor_elems_btn, 5, 3, 1,
+        self.select_more_monitor_elems_btn.setObjectName(
+            "select_more_monitor_elems_btn")
+        self.scan_gridLayout.addWidget(self.select_more_monitor_elems_btn, 4,
+                                       4, 1, 1)
+        self.select_monitor_elem_btn = QtWidgets.QPushButton(
+            self.scan_groupBox)
+        self.select_monitor_elem_btn.setObjectName("select_monitor_elem_btn")
+        self.scan_gridLayout.addWidget(self.select_monitor_elem_btn, 3, 4, 1,
                                        1)
-        self.monitor_vars_lineEdit = QtWidgets.QLineEdit(self.scan_groupBox)
-        self.monitor_vars_lineEdit.setPlaceholderText("")
-        self.monitor_vars_lineEdit.setObjectName("monitor_vars_lineEdit")
-        self.scan_gridLayout.addWidget(self.monitor_vars_lineEdit, 4, 1, 1, 3)
-        self.copy_pvname_btn = QtWidgets.QPushButton(self.scan_groupBox)
-        self.copy_pvname_btn.setObjectName("copy_pvname_btn")
-        self.scan_gridLayout.addWidget(self.copy_pvname_btn, 1, 4, 1, 1)
-        self.scan_verticalLayout.addLayout(self.scan_gridLayout)
+        self.other_monitors_counter_lbl = QtWidgets.QLabel(self.scan_groupBox)
+        self.other_monitors_counter_lbl.setObjectName(
+            "other_monitors_counter_lbl")
+        self.scan_gridLayout.addWidget(self.other_monitors_counter_lbl, 4, 0,
+                                       1, 2)
+        self.show_other_monitors_btn = QtWidgets.QPushButton(
+            self.scan_groupBox)
+        self.show_other_monitors_btn.setObjectName("show_other_monitors_btn")
+        self.scan_gridLayout.addWidget(self.show_other_monitors_btn, 4, 2, 1,
+                                       2)
+        self.gridLayout.addLayout(self.scan_gridLayout, 0, 0, 1, 1)
         self.scanlog_verticalLayout = QtWidgets.QVBoxLayout()
         self.scanlog_verticalLayout.setObjectName("scanlog_verticalLayout")
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
@@ -160,8 +154,7 @@ class Ui_MainWindow(object):
         self.scan_log_textEdit.setFont(font)
         self.scan_log_textEdit.setObjectName("scan_log_textEdit")
         self.scanlog_verticalLayout.addWidget(self.scan_log_textEdit)
-        self.scan_verticalLayout.addLayout(self.scanlog_verticalLayout)
-        self.gridLayout.addLayout(self.scan_verticalLayout, 0, 0, 1, 1)
+        self.gridLayout.addLayout(self.scanlog_verticalLayout, 1, 0, 1, 1)
         self.daq_groupBox = QtWidgets.QGroupBox(self.splitter)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
                                            QtWidgets.QSizePolicy.Preferred)
@@ -315,12 +308,14 @@ class Ui_MainWindow(object):
             "                                      stop: 0 #EDECEB, stop: 1 #FFFFFF);\n"
             "}")
         self.plot_groupBox.setObjectName("plot_groupBox")
-        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.plot_groupBox)
-        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.gridLayout_4 = QtWidgets.QGridLayout(self.plot_groupBox)
+        self.gridLayout_4.setObjectName("gridLayout_4")
         self.scan_plot_widget = MatplotlibErrorbarWidget(self.plot_groupBox)
         self.scan_plot_widget.setFigureAutoScale(True)
         self.scan_plot_widget.setObjectName("scan_plot_widget")
-        self.verticalLayout_3.addWidget(self.scan_plot_widget)
+        self.gridLayout_4.addWidget(self.scan_plot_widget, 0, 0, 1, 1)
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
         self.save_data_btn = QtWidgets.QPushButton(self.plot_groupBox)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
                                            QtWidgets.QSizePolicy.Fixed)
@@ -330,7 +325,15 @@ class Ui_MainWindow(object):
             self.save_data_btn.sizePolicy().hasHeightForWidth())
         self.save_data_btn.setSizePolicy(sizePolicy)
         self.save_data_btn.setObjectName("save_data_btn")
-        self.verticalLayout_3.addWidget(self.save_data_btn)
+        self.horizontalLayout_3.addWidget(self.save_data_btn)
+        self.pushButton = QtWidgets.QPushButton(self.plot_groupBox)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout_3.addWidget(self.pushButton)
+        spacerItem = QtWidgets.QSpacerItem(40, 20,
+                                           QtWidgets.QSizePolicy.Expanding,
+                                           QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem)
+        self.gridLayout_4.addLayout(self.horizontalLayout_3, 1, 0, 1, 1)
         self.gridLayout_3.addWidget(self.splitter_2, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
@@ -353,12 +356,15 @@ class Ui_MainWindow(object):
         self.actionAbout_Qt.setObjectName("actionAbout_Qt")
         self.actionQuad_Scan = QtWidgets.QAction(MainWindow)
         self.actionQuad_Scan.setObjectName("actionQuad_Scan")
+        self.actionLoad_Lattice = QtWidgets.QAction(MainWindow)
+        self.actionLoad_Lattice.setObjectName("actionLoad_Lattice")
         self.menu_File.addAction(self.actionE_xit)
         self.menu_Help.addAction(self.actionContents)
         self.menu_Help.addSeparator()
         self.menu_Help.addAction(self.actionAbout)
         self.menu_Help.addAction(self.actionAbout_Qt)
         self.menuTools.addAction(self.actionQuad_Scan)
+        self.menuTools.addAction(self.actionLoad_Lattice)
         self.menubar.addAction(self.menu_File.menuAction())
         self.menubar.addAction(self.menuTools.menuAction())
         self.menubar.addAction(self.menu_Help.menuAction())
@@ -369,6 +375,8 @@ class Ui_MainWindow(object):
         self.actionE_xit.triggered.connect(MainWindow.close)
         self.actionQuad_Scan.triggered.connect(MainWindow.onQuadScanAction)
         self.actionContents.triggered.connect(MainWindow.onHelp)
+        self.actionLoad_Lattice.triggered.connect(
+            MainWindow.onLoadLatticeAction)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
@@ -376,16 +384,20 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.scan_groupBox.setTitle(
             _translate("MainWindow", "Scan Configuration"))
-        self.scan_vars_get_lineEdit.setToolTip(
+        self.select_alter_elem_btn.setText(_translate("MainWindow", "Select"))
+        self.label_9.setText(_translate("MainWindow", "Alter Ranges"))
+        self.monitor_elem_lineEdit.setToolTip(
             _translate(
                 "MainWindow",
-                "<html><head/><body><p>Variables to scan, if selected \'PV\' mode, input readback PV name.</p></body></html>"
+                "<html><head/><body><p>Variables to monitor</p></body></html>")
+        )
+        self.upper_limit_lineEdit.setToolTip(
+            _translate(
+                "MainWindow",
+                "<html><head/><body><p>Up limit of scan range</p></body></html>"
             ))
-        self.scan_vars_get_lineEdit.setText(
-            _translate("MainWindow", "VA:LS1_BTS:QH_D1942:I_RD"))
-        self.scan_range_set_btn.setText(_translate("MainWindow", "Range"))
-        self.label_8.setText(_translate("MainWindow", "Alter Vars (set)"))
-        self.label_9.setText(_translate("MainWindow", "Alter Ranges"))
+        self.upper_limit_lineEdit.setText(_translate("MainWindow", "100"))
+        self.label_11.setText(_translate("MainWindow", "Monitors"))
         self.label_10.setText(_translate("MainWindow", "To"))
         self.lower_limit_lineEdit.setToolTip(
             _translate(
@@ -393,51 +405,15 @@ class Ui_MainWindow(object):
                 "<html><head/><body><p>Lower limit of scan range</p></body></html>"
             ))
         self.lower_limit_lineEdit.setText(_translate("MainWindow", "-100"))
-        self.upper_limit_lineEdit.setToolTip(
-            _translate(
-                "MainWindow",
-                "<html><head/><body><p>Up limit of scan range</p></body></html>"
-            ))
-        self.upper_limit_lineEdit.setText(_translate("MainWindow", "100"))
-        self.select_scan_elems_btn.setText(_translate("MainWindow", "Select"))
-        self.label_11.setText(_translate("MainWindow", "Monitor Vars"))
-        self.label_13.setText(_translate("MainWindow", "Alter Vars (get)"))
-        self.scan_vars_type_cbb.setToolTip(
-            _translate(
-                "MainWindow",
-                "<html><head/><body><p>Type of scan variables: PV or Element</p></body></html>"
-            ))
-        self.scan_vars_type_cbb.setItemText(0, _translate("MainWindow", "PV"))
-        self.scan_vars_type_cbb.setItemText(1,
-                                            _translate("MainWindow",
-                                                       "Element"))
-        self.scan_vars_put_lineEdit.setToolTip(
-            _translate(
-                "MainWindow",
-                "<html><head/><body><p>Variables to scan, if selected \'PV\' mode, input setpoint PV name.</p></body></html>"
-            ))
-        self.scan_vars_put_lineEdit.setText(
-            _translate("MainWindow", "VA:LS1_BTS:QH_D1942:I_CSET"))
-        self.monitor_vars_type_cbb.setToolTip(
-            _translate(
-                "MainWindow",
-                "<html><head/><body><p>Type of monitor variables: PV or Element</p></body></html>"
-            ))
-        self.monitor_vars_type_cbb.setItemText(0, _translate(
-            "MainWindow", "PV"))
-        self.monitor_vars_type_cbb.setItemText(1,
-                                               _translate(
-                                                   "MainWindow", "Element"))
-        self.select_monitor_elems_btn.setText(
+        self.label_8.setText(_translate("MainWindow", "Alter Element"))
+        self.select_more_monitor_elems_btn.setText(
+            _translate("MainWindow", "More"))
+        self.select_monitor_elem_btn.setText(
             _translate("MainWindow", "Select"))
-        self.monitor_vars_lineEdit.setToolTip(
-            _translate(
-                "MainWindow",
-                "<html><head/><body><p>Variables to monitor</p></body></html>")
-        )
-        self.monitor_vars_lineEdit.setText(
-            _translate("MainWindow", "VA:LS1_BTS:PM_D2056:XRMS_RD"))
-        self.copy_pvname_btn.setText(_translate("MainWindow", "Copy"))
+        self.other_monitors_counter_lbl.setText(
+            _translate("MainWindow", "Other Selected Monitors (0)"))
+        self.show_other_monitors_btn.setText(
+            _translate("MainWindow", "Show Monitors"))
         self.label_12.setText(_translate("MainWindow", "Event Log"))
         self.daq_groupBox.setTitle(
             _translate("MainWindow", "DAQ Configuration"))
@@ -455,6 +431,7 @@ class Ui_MainWindow(object):
         self.plot_groupBox.setTitle(
             _translate("MainWindow", "Correlation Plot"))
         self.save_data_btn.setText(_translate("MainWindow", "Save Data"))
+        self.pushButton.setText(_translate("MainWindow", "Auto Labels"))
         self.menu_File.setTitle(_translate("MainWindow", "&File"))
         self.menu_Help.setTitle(_translate("MainWindow", "&Help"))
         self.menuTools.setTitle(_translate("MainWindow", "&Tools"))
@@ -467,6 +444,8 @@ class Ui_MainWindow(object):
         self.actionAbout_Qt.setText(_translate("MainWindow", "About Qt"))
         self.actionQuad_Scan.setText(
             _translate("MainWindow", "Quad Scan Analysis"))
+        self.actionLoad_Lattice.setText(
+            _translate("MainWindow", "Load Lattice"))
 
 
 from mpl4qt.widgets.mplerrorbarwidget import MatplotlibErrorbarWidget
