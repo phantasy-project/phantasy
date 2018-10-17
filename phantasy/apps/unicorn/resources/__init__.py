aa0a834a596d31ae18db31918c2b5ea3d8c47ea6
Tong Zhang <zhangt@frib.msu.edu>
2018-10-17 14:52:29 -0400

    ENH: Add toolbar icons for unicorn app.

---
+++ phantasy/apps/unicorn/resources/__init__.py
@@ -0,0 +1,7 @@
+from .app_icon import unicorn_icon
+from .zoom_in_icon import icon as zoom_in_icon
+from .zoom_out_icon import icon as zoom_out_icon
+from .back_icon import icon as back_icon
+from .forward_icon import icon as forward_icon
+from .quit_icon import icon as quit_icon
+from .home_icon import icon as home_icon
