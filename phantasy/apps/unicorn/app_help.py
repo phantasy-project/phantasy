b3390cc1fad3024ab218a9365e00ecbbc9b1d126
Tong Zhang <zhangt@frib.msu.edu>
2018-10-17 15:31:59 -0400

    ENH/UA: Add shortcuts manual to help page.
    UA: Unicorn App

--- phantasy/apps/unicorn/app_help.py
+++ phantasy/apps/unicorn/app_help.py
@@ -6,26 +6,68 @@ from .ui_app_help import Ui_Dialog
 
 
 HELP_QNA = '''
-<html>
+<div>
 <p><strong>What is Unicorn App?</strong></p>
+</div>
+<div>
 <p><em>Unicorn App</em> is a GUI application built upon PyQt5, features easy access and control to the <em>UNICORN</em> service, which is a web service to interpret the units between engineering and physics fields, typically for the accelerator system.</p>
+</div>
+<div>
 <p><strong>How to start up Unicorn App?</strong></p>
+</div>
+<div>
 <p>Search <em>Unicorn App </em>in the search box of Gnome's Activities, or locate it in the system menu, probably under the Other category.</p>
+</div>
+<div>
 <p><strong>How to initialize the database?</strong></p>
+</div>
+<div>
 <p>If for the first time <em>Unicorn App</em> is started, there might be the issue like the database cannot be located, thus 'Internal Server Error' happens, which could be resolved by resetting the database: <span style="color: #ff0000;">Edit</span> &rarr; <span style="color: #ff0000;">Preferences</span>&nbsp;&rarr; <span style="color: #ff0000;">Unicorn WebApp</span> &rarr; <span style="color: #ff0000;">Database</span>, push <span style="color: #ff0000;">Reset</span> button, and push <span style="color: #ff0000;">OK</span> button.</p>
+</div>
+<div>
 <p><strong>How to update database?</strong></p>
+</div>
+<div>
 <p>The database may need to update when unicorn-webapp is updated, simply push <span style="color: #ff0000;">Reset</span> database button, keep in mind that all user changes will be gone, e.g. invoked count.</p>
+</div>
+<div>
 <p><strong>Internal Server Error?</strong></p>
+</div>
+<div>
 <p>See <em>How to initialize the database</em>?</p>
+</div>
+<div>
 <p><strong>Change the font size?</strong></p>
+</div>
+<div>
 <p>This could be achieved by change the page zoom factor: <span style="color: #ff0000;">Edit</span> &rarr; <span style="color: #ff0000;">Preferences</span>&nbsp;&rarr; <span style="color: #ff0000;">Style</span> &rarr; <span style="color: #ff0000;">Page Zoom</span>, change the value that fit your screen, push <span style="color: #ff0000;">OK</span> to apply.</p>
+</div>
+<div>
 <p><strong>Start UNICORN at another port?</strong></p>
+</div>
+<div>
 <p>By default, the UNICORN service is started with random port number, if the user wanna start with some other port, the following procedure should work: <span style="color: #ff0000;">Edit</span> &rarr; <span style="color: #ff0000;">Preferences</span>&nbsp;&rarr; <span style="color: #ff0000;">Unicorn WebApp</span> &rarr; <span style="color: #ff0000;">Service Status</span>, push <span style="color: #ff0000;">STOP</span> button to stop the current one, and change the port number at <span style="color: #ff0000;">Base URL</span> line, then Push <span style="color: #ff0000;">START</span> and <span style="color: #ff0000;">OK</span>.</p>
+</div>
+<div>
 <p><strong>Stop current UNICORN service?</strong></p>
+</div>
+<div>
 <p><span style="color: #ff0000;">Edit</span> &rarr; <span style="color: #ff0000;">Preferences</span>&nbsp;&rarr; <span style="color: #ff0000;">Unicorn WebApp</span> &rarr; <span style="color: #ff0000;">Service Status</span>, push <span style="color: #ff0000;">STOP</span> button and <span style="color: #ff0000;">OK</span>.</p>
+</div>
+<div>
 <p><strong>Clean up residual services?</strong></p>
+</div>
+<div>
 <p>Sometimes, the started UNICORN service has not been stopped properly, clean up could be done by <span style="color: #ff0000;">Edit</span> &rarr; <span style="color: #ff0000;">Preferences</span>&nbsp;&rarr; <span style="color: #ff0000;">Advance</span>, push <span style="color: #ff0000;">Update</span> button to see all alive services, and push <span style="color: #ff0000;">Clean Up</span> to stop all but the current working one, if push <span style="color: #ff0000;">Update</span> button again, there should be only the current one alive.</p>
-</html>
+<p><strong>How to use keyboard cuts?</strong></p>
+<p><span style="background-color: #ff6600;">CTRL + W</span>: Exit application;</p>
+<p><span style="background-color: #ff6600;">CTRL + 0</span>: Reset page scale to default;</p>
+<p><span style="background-color: #ff6600;">CTRL + SHIFT + =</span>: Page zoom in by 5%;</p>
+<p><span style="background-color: #ff6600;">CTRL + -</span>: Page zoom out by 5%;</p>
+<p><span style="background-color: #ff6600;">ALT + &rarr;</span>: Go forward;</p>
+<p><span style="background-color: #ff6600;">ALT + &larr;</span>: Go back;</p>
+<p><span style="background-color: #ff6600;">CTRL + h</span>: Go to home page;</p>
+</div>
 '''
 
 HELP_LIC = '''
