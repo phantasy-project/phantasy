689aab85e7a7565fcca4f1b7d02df597df019b2e
Tong Zhang <zhangt@frib.msu.edu>
2018-10-17 22:47:22 -0400

    MNT: Bump Correlation Visualizer app to 1.1.

--- phantasy/apps/correlation_visualizer/__init__.py
+++ phantasy/apps/correlation_visualizer/__init__.py
@@ -9,14 +9,14 @@ __authors__ = "Tong Zhang"
 __copyright__ = "(c) 2018, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
-__version__ = '1.0'
+__version__ = '1.1'
 
 
 def run(cli=False):
     app = QApplication(sys.argv)
     w = CorrelationVisualizerWindow(version=__version__)
     w.show()
-    w.setWindowTitle("Correlation Visualizer")
+    w.setWindowTitle("Parameters Correlation Analysis and Visualization")
     if cli:
         app.exec_()
     else:
