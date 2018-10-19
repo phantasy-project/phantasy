bba3146c520bd86ad006fcc3ea0fb256ca2ff7b7
Tong Zhang <zhangt@frib.msu.edu>
2018-10-19 11:40:48 -0400

    UI/CV: Add icons for toolbuttons.

--- phantasy/apps/correlation_visualizer/icons/__init__.py
+++ phantasy/apps/correlation_visualizer/icons/__init__.py
@@ -1,1 +1,6 @@
 from .cv_icon import icon as cv_icon
+from .save import icon as save_icon
+from .xylabel import icon as xylabel_icon
+from .title import icon as title_icon
+from .moveto import icon as moveto_icon
+from .pushbutton import icon as set_icon
