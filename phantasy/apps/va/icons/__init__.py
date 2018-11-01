e26b7f5d38fd073d0c95c92a15e11633400f2354
Tong Zhang <zhangt@frib.msu.edu>
2018-11-01 16:30:34 -0400

    UI/VA: Add icons for notebook run/stop.

--- phantasy/apps/va/icons/__init__.py
+++ phantasy/apps/va/icons/__init__.py
@@ -1,3 +1,5 @@
 from .frib_va import icon as va_icon
 from .play import icon as run_icon
 from .stop import icon as stop_icon
+from .notebook_run import icon as nb_run_icon
+from .notebook_stop import icon as nb_stop_icon
