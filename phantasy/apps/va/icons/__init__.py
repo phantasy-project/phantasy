79aaa83b14d8bb590fb56882fb0fad533c786f2a
Tong Zhang <zhangt@frib.msu.edu>
2018-11-01 16:47:51 -0400

    UI/VA: Add icon for va info btn.

--- phantasy/apps/va/icons/__init__.py
+++ phantasy/apps/va/icons/__init__.py
@@ -3,3 +3,4 @@ from .play import icon as run_icon
 from .stop import icon as stop_icon
 from .notebook_run import icon as nb_run_icon
 from .notebook_stop import icon as nb_stop_icon
+from .info import icon as info_icon
