166e6ef8658f73e159f59d67c8de47a9a0077772
Tong Zhang <zhangt@frib.msu.edu>
2018-11-09 12:59:52 -0500

    UI/CV: Add config wigdet for MPS guardian.

---
+++ phantasy/apps/correlation_visualizer/ui/ui_mps_config.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui_mps_config.ui'
+#
+# Created by: PyQt5 UI code generator 5.5.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(456, 138)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setObjectName("gridLayout")
+        self.label = QtWidgets.QLabel(Dialog)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                           QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.verticalLayout.addLayout(self.gridLayout)
+        self.mps_pv_name_cbb = QtWidgets.QComboBox(Dialog)
+        self.mps_pv_name_cbb.setEditable(True)
+        self.mps_pv_name_cbb.setObjectName("mps_pv_name_cbb")
+        self.mps_pv_name_cbb.addItem("")
+        self.mps_pv_name_cbb.addItem("")
+        self.verticalLayout.addWidget(self.mps_pv_name_cbb)
+        spacerItem = QtWidgets.QSpacerItem(20, 154,
+                                           QtWidgets.QSizePolicy.Minimum,
+                                           QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20,
+                                            QtWidgets.QSizePolicy.Expanding,
+                                            QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        self.cancel_btn = QtWidgets.QPushButton(Dialog)
+        self.cancel_btn.setObjectName("cancel_btn")
+        self.horizontalLayout.addWidget(self.cancel_btn)
+        self.ok_btn = QtWidgets.QPushButton(Dialog)
+        self.ok_btn.setObjectName("ok_btn")
+        self.horizontalLayout.addWidget(self.ok_btn)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.retranslateUi(Dialog)
+        self.cancel_btn.clicked.connect(Dialog.on_click_cancel)
+        self.ok_btn.clicked.connect(Dialog.on_click_ok)
+        self.mps_pv_name_cbb.editTextChanged['QString'].connect(
+            Dialog.on_update_pvname)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "MPS Configuration"))
+        self.label.setText(_translate("Dialog", "PV Name of MPS Status"))
+        self.mps_pv_name_cbb.setItemText(
+            0, _translate("Dialog", "MPS_FPS:MSTR_N0001:MpsStatus"))
+        self.mps_pv_name_cbb.setItemText(1,
+                                         _translate("Dialog",
+                                                    "VA:SVR:MpsStatus"))
+        self.cancel_btn.setText(_translate("Dialog", "Cancel"))
+        self.ok_btn.setText(_translate("Dialog", "OK"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
