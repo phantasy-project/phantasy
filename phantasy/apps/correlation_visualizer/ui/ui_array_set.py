ff795c899b7ee29e44cb571eb4a5bd09605ecad1
Tong Zhang <zhangt@frib.msu.edu>
2018-11-10 07:10:16 -0500

    UI/CV: Add support to sort input alter array.

--- phantasy/apps/correlation_visualizer/ui/ui_array_set.py
+++ phantasy/apps/correlation_visualizer/ui/ui_array_set.py
@@ -39,6 +39,44 @@ class Ui_Dialog(object):
         self.generate_btn.setObjectName("generate_btn")
         self.horizontalLayout.addWidget(self.generate_btn)
         self.verticalLayout.addLayout(self.horizontalLayout)
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setContentsMargins(-1, 0, -1, -1)
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        spacerItem = QtWidgets.QSpacerItem(40, 20,
+                                           QtWidgets.QSizePolicy.Expanding,
+                                           QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem)
+        self.sort_asc_tbtn = QtWidgets.QToolButton(Dialog)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
+                                           QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.sort_asc_tbtn.sizePolicy().hasHeightForWidth())
+        self.sort_asc_tbtn.setSizePolicy(sizePolicy)
+        icon = QtGui.QIcon()
+        icon.addPixmap(
+            QtGui.QPixmap(":/icons/sort-asc.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.sort_asc_tbtn.setIcon(icon)
+        self.sort_asc_tbtn.setObjectName("sort_asc_tbtn")
+        self.horizontalLayout_3.addWidget(self.sort_asc_tbtn)
+        self.sort_desc_tbtn = QtWidgets.QToolButton(Dialog)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
+                                           QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.sort_desc_tbtn.sizePolicy().hasHeightForWidth())
+        self.sort_desc_tbtn.setSizePolicy(sizePolicy)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(
+            QtGui.QPixmap(":/icons/sort-desc.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.sort_desc_tbtn.setIcon(icon1)
+        self.sort_desc_tbtn.setObjectName("sort_desc_tbtn")
+        self.horizontalLayout_3.addWidget(self.sort_desc_tbtn)
+        self.verticalLayout.addLayout(self.horizontalLayout_3)
         self.data_textEdit = QtWidgets.QTextEdit(Dialog)
         self.data_textEdit.setReadOnly(False)
         self.data_textEdit.setObjectName("data_textEdit")
@@ -55,10 +93,10 @@ class Ui_Dialog(object):
         self.import_btn.setSizePolicy(sizePolicy)
         self.import_btn.setObjectName("import_btn")
         self.horizontalLayout_2.addWidget(self.import_btn)
-        spacerItem = QtWidgets.QSpacerItem(40, 20,
-                                           QtWidgets.QSizePolicy.Expanding,
-                                           QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout_2.addItem(spacerItem)
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20,
+                                            QtWidgets.QSizePolicy.Expanding,
+                                            QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem1)
         self.cancel_btn = QtWidgets.QPushButton(Dialog)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
                                            QtWidgets.QSizePolicy.Fixed)
@@ -83,7 +121,18 @@ class Ui_Dialog(object):
 
         self.retranslateUi(Dialog)
         self.data_textEdit.textChanged.connect(Dialog.on_text_changed)
+        self.sort_asc_tbtn.clicked.connect(Dialog.on_sort_array)
+        self.sort_desc_tbtn.clicked.connect(Dialog.on_sort_array)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
+        Dialog.setTabOrder(self.vfrom_lineEdit, self.vto_lineEdit)
+        Dialog.setTabOrder(self.vto_lineEdit, self.vstep_lineEdit)
+        Dialog.setTabOrder(self.vstep_lineEdit, self.generate_btn)
+        Dialog.setTabOrder(self.generate_btn, self.sort_asc_tbtn)
+        Dialog.setTabOrder(self.sort_asc_tbtn, self.sort_desc_tbtn)
+        Dialog.setTabOrder(self.sort_desc_tbtn, self.data_textEdit)
+        Dialog.setTabOrder(self.data_textEdit, self.import_btn)
+        Dialog.setTabOrder(self.import_btn, self.cancel_btn)
+        Dialog.setTabOrder(self.cancel_btn, self.ok_btn)
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
@@ -92,6 +141,18 @@ class Ui_Dialog(object):
         self.label_2.setText(_translate("Dialog", "To"))
         self.label_3.setText(_translate("Dialog", "of Step"))
         self.generate_btn.setText(_translate("Dialog", "Generate"))
+        self.sort_asc_tbtn.setToolTip(
+            _translate(
+                "Dialog",
+                "<html><head/><body><p> Sort array ascendingly</p></body></html>"
+            ))
+        self.sort_asc_tbtn.setText(_translate("Dialog", "Sort"))
+        self.sort_desc_tbtn.setToolTip(
+            _translate(
+                "Dialog",
+                "<html><head/><body><p>Sort array descendingly</p></body></html>"
+            ))
+        self.sort_desc_tbtn.setText(_translate("Dialog", "Sort"))
         self.data_textEdit.setToolTip(
             _translate(
                 "Dialog",
@@ -106,6 +167,8 @@ class Ui_Dialog(object):
         self.cancel_btn.setText(_translate("Dialog", "Cancel"))
         self.ok_btn.setText(_translate("Dialog", "OK"))
 
+
+from . import resources_rc
 
 if __name__ == "__main__":
     import sys
