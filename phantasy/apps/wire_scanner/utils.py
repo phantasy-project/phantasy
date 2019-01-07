1c024d44f72713cbef13398415c1a75948eead75
Tong Zhang <zhangt@frib.msu.edu>
2019-01-07 11:18:26 -0500

    ENH: Add function to find the wire-scanner conf file.

--- phantasy/apps/wire_scanner/utils.py
+++ phantasy/apps/wire_scanner/utils.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import os
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -36,3 +37,22 @@ def wait(pv, goal, timeout=60):
     except Empty:
         _LOGGER.warning("Timeout, no changes detected.")
         pv.remove_callback(cid)
+
+
+def find_dconf():
+    """Find parameter configuration file for wire-scanners.
+    searching the following locations:
+    * ~/.phantasy/ws.ini
+    * /etc/phantasy/ws.ini
+    * package location: apps/wire_scanner/config/ws.ini
+    """
+    home_conf = os.path.expanduser('~/.phantasy/ws.ini')
+    sys_conf = '/etc/phantasy/ws.ini'
+    if os.path.isfile(home_conf):
+        return home_conf
+    elif os.path.isfile(sys_conf):
+        return sys_conf
+    else:
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        return os.path.join(basedir, 'config/ws.ini')
+
