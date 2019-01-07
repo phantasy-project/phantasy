2bb144482f6f46e5fa58ec45985983a72eaa1720
Tong Zhang <zhangt@frib.msu.edu>
2019-01-07 16:11:38 -0500

    ENH/WS: Add function to run wire_scanner app.

---
+++ phantasy/apps/wire_scanner/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf8 -*-
+
+import sys
+from PyQt5.QtWidgets import QApplication
+
+from .app import WireScannerWindow
+
+__authors__ = "Tong Zhang"
+__copyright__ = "(c) 2019, Facility for Rare Isotope beams," \
+                " Michigan State University"
+__contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
+__version__ = '0.1'
+
+
+def run(cli=False):
+    app = QApplication(sys.argv)
+    w = WireScannerWindow(version=__version__)
+    w.show()
+    w.setWindowTitle("Wire Scanner (Profile Monitor) Application")
+    if cli:
+        app.exec_()
+    else:
+        sys.exit(app.exec_())
