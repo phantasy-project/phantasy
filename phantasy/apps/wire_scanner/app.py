0eea81689b2e66a1a43390eb69a657d98678a2b3
Tong Zhang <zhangt@frib.msu.edu>
2019-01-08 15:13:12 -0500

    ENH/WS: Implement Data (Sync/Load/Save/Converter) menu.

--- phantasy/apps/wire_scanner/app.py
+++ phantasy/apps/wire_scanner/app.py
@@ -3,11 +3,14 @@
 
 """GUI App for wire-scanner.
 """
-from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtCore import QThread
-from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QDesktopServices
 from PyQt5.QtGui import QDoubleValidator
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QMessageBox
 
 from functools import partial
 
@@ -16,6 +19,8 @@ from phantasy import MachinePortal
 from phantasy import Configuration
 from phantasy.apps.wire_scanner.utils import find_dconf
 from phantasy.apps.wire_scanner.device import Device
+from phantasy.apps.utils import get_open_filename
+from phantasy.apps.utils import get_save_filename
 
 from .app_utils import DeviceRunner
 from .ui.ui_app import Ui_MainWindow
@@ -53,19 +58,27 @@ class WireScannerWindow(BaseAppForm, Ui_MainWindow):
 
         # variable initialization
         self._ws_device = None
-        self._device_mode = "doSomethingReal"
+        self._device_mode = "live"
+        # widgets
+        self._data_converter_dlg = None
 
         # events
         self.pm_names_cbb.currentTextChanged.connect(self.on_pm_name_changed)
         for o in self.controls_groupBox.findChildren(QLineEdit):
             o.textChanged.connect(self.highlight_text)
 
+        self.start_pos1_lineEdit.textChanged.connect(self.startpos1_lineEdit.setText)
+        self.start_pos2_lineEdit.textChanged.connect(self.startpos2_lineEdit.setText)
+        self.stop_pos1_lineEdit.textChanged.connect(self.stoppos1_lineEdit.setText)
+        self.stop_pos2_lineEdit.textChanged.connect(self.stoppos2_lineEdit.setText)
+
         # validator
         for o in (self.start_pos1_lineEdit, self.start_pos2_lineEdit,
                   self.stop_pos1_lineEdit, self.stop_pos2_lineEdit,
                   self.offset1_lineEdit, self.offset2_lineEdit,
                   self.offset3_lineEdit, self.outlimit_lineEdit,
-                  self.start_pos_lineEdit, self.stop_pos_lineEdit):
+                  self.startpos1_lineEdit, self.startpos2_lineEdit,
+                  self.stoppos1_lineEdit, self.stoppos2_lineEdit):
             o.setValidator(QDoubleValidator())
 
         # init ui
@@ -226,13 +239,77 @@ class WireScannerWindow(BaseAppForm, Ui_MainWindow):
         if f:
             self._device_mode = "simulation"
         else:
-            self._device_mode = "doSomethingReal"
+            self._device_mode = "live"
 
     @pyqtSlot()
     def on_emstop_device(self):
         """Emergency stop running device.
         """
         self.device_runner.stop()
+
+    @pyqtSlot()
+    def on_load_data(self):
+        """Open file to load previous saved measured data.
+        """
+        print("Load data from file")
+        filepath, ext = get_open_filename(self,
+                filter="JSON Files (*.json)")
+        if filepath is None:
+            return
+        try:
+            self._ws_device.sync_data(mode='file', filename=filepath)
+        except:
+            QMessageBox.critical(self, "Load Data",
+                    "Failed to load data from {}".format(filepath),
+                    QMessageBox.Ok)
+        else:
+            QMessageBox.information(self, "Load Data",
+                    "Successfully loaded data from {}".format(filepath),
+                    QMessageBox.Ok)
+
+    @pyqtSlot()
+    def on_save_data(self):
+        """Save data into file.
+        """
+        print("Save data to file")
+        filepath, ext = get_save_filename(self,
+                filter="JSON Files (*.json)")
+        if filepath is None:
+            return
+        try:
+            self._ws_device.save_data(filepath)
+        except:
+            QMessageBox.critical(self, "Save Data",
+                    "Failed to save data to {}".format(filepath),
+                    QMessageBox.Ok)
+        else:
+            QMessageBox.information(self, "Save Data",
+                    "Successfully saved data to {}".format(filepath),
+                    QMessageBox.Ok)
+
+    @pyqtSlot()
+    def on_sync_data(self):
+        """Sync data from controls PV.
+        """
+        print("Sync data from PVs")
+        if self._ws_device is None:
+            return
+        self._ws_device.sync_data(mode='live')
+
+    @pyqtSlot()
+    def on_dat2json(self):
+        """Convert data file from old .dat to new .json format.
+        """
+        from .app_dat2json import Dat2JsonDialog
+        self._data_converter_dlg = Dat2JsonDialog()
+        self._data_converter_dlg.show()
+
+    @pyqtSlot()
+    def on_locate_config(self):
+        """Locate (find and open) device configuration file path.
+        """
+        path = self._dconf.config_path
+        QDesktopServices.openUrl(QUrl(path))
 
     def get_all_pms(self):
         """Return all PM elements.
