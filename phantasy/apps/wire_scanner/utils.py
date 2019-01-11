73ea56a67bb6eac6d1851f55ade643ac24290f8b
Tong Zhang <zhangt@frib.msu.edu>
2019-01-10 20:58:00 -0500

    ENH/WS: Add function to find and load mpl settings.

--- phantasy/apps/wire_scanner/utils.py
+++ phantasy/apps/wire_scanner/utils.py
@@ -4,6 +4,8 @@
 import logging
 import os
 
+from mpl4qt.widgets.utils import MatplotlibCurveWidgetSettings
+
 _LOGGER = logging.getLogger(__name__)
 
 try:
@@ -56,3 +58,36 @@ def find_dconf():
         basedir = os.path.abspath(os.path.dirname(__file__))
         return os.path.join(basedir, 'config/ws.ini')
 
+
+def find_mplconf():
+    """Find configuration file (JSON) for matplotlibcurvewidget,
+    searching the following locations:
+    * ~/.phantasy/mpl_settings_ws.json
+    * /etc/phanasy/mpl_settings_ws.json
+    * package root location for this apps/wire_scanner/config/mpl_settings_ws.json
+    """
+    home_conf = os.path.expanduser('~/.phantasy/mpl_settings_ws.json')
+    sys_conf = '/etc/phantasy/mpl_settings_ws.json'
+    if os.path.isfile(home_conf):
+        return home_conf
+    elif os.path.isfile(sys_conf):
+        return sys_conf
+    else:
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        return os.path.join(basedir, 'config/mpl_settings_ws.json')
+
+
+def apply_mplcurve_settings(mplcurveWidget, json_path=None):
+    """Apply JSON settings read from *json_path* to *mplcurveWidget*.
+
+    Parameters
+    ----------
+    mplcurveWidget : MatplotlibCurveWidget
+        Instance of MatplotlibCurveWidget.
+    json_path : str
+        Path of JSON settings file.
+    """
+    if json_path is None:
+        json_path = find_mplconf()
+    s = MatplotlibCurveWidgetSettings(json_path)
+    mplcurveWidget.apply_mpl_settings(s)
