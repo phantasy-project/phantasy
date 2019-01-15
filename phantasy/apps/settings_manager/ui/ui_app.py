754aaf35a8cb9c63c87eb31a25750a0c69e097cd
Tong Zhang <zhangt@frib.msu.edu>
2019-01-14 20:29:41 -0500

    UI/SM: Update main app ui and resources.

--- phantasy/apps/settings_manager/ui/ui_app.py
+++ phantasy/apps/settings_manager/ui/ui_app.py
@@ -20,18 +20,75 @@ class Ui_MainWindow(object):
         self.treeView = QtWidgets.QTreeView(self.centralwidget)
         self.treeView.setObjectName("treeView")
         self.gridLayout.addWidget(self.treeView, 0, 0, 1, 1)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setContentsMargins(-1, 0, -1, -1)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem = QtWidgets.QSpacerItem(40, 20,
+                                           QtWidgets.QSizePolicy.Expanding,
+                                           QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.apply_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.apply_btn.setObjectName("apply_btn")
+        self.horizontalLayout.addWidget(self.apply_btn)
+        self.gridLayout.addLayout(self.horizontalLayout, 1, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 30))
         self.menubar.setObjectName("menubar")
+        self.menu_File = QtWidgets.QMenu(self.menubar)
+        self.menu_File.setObjectName("menu_File")
+        self.menuEdit = QtWidgets.QMenu(self.menubar)
+        self.menuEdit.setObjectName("menuEdit")
         MainWindow.setMenuBar(self.menubar)
+        self.actionSave = QtWidgets.QAction(MainWindow)
+        icon = QtGui.QIcon()
+        icon.addPixmap(
+            QtGui.QPixmap(":/icons/save.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.actionSave.setIcon(icon)
+        self.actionSave.setObjectName("actionSave")
+        self.action_load_from_snp = QtWidgets.QAction(MainWindow)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(
+            QtGui.QPixmap(":/icons/load.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.action_load_from_snp.setIcon(icon1)
+        self.action_load_from_snp.setObjectName("action_load_from_snp")
+        self.actionE_xit = QtWidgets.QAction(MainWindow)
+        self.actionE_xit.setObjectName("actionE_xit")
+        self.menu_File.addAction(self.actionSave)
+        self.menu_File.addAction(self.action_load_from_snp)
+        self.menu_File.addSeparator()
+        self.menu_File.addAction(self.actionE_xit)
+        self.menubar.addAction(self.menu_File.menuAction())
+        self.menubar.addAction(self.menuEdit.menuAction())
 
         self.retranslateUi(MainWindow)
+        self.actionSave.triggered.connect(MainWindow.on_save)
+        self.action_load_from_snp.triggered.connect(
+            MainWindow.on_load_from_snp)
+        self.apply_btn.clicked.connect(MainWindow.on_apply_settings)
+        self.actionE_xit.triggered.connect(MainWindow.close)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.apply_btn.setToolTip(
+            _translate(
+                "MainWindow",
+                "<html><head/><body><p>Apply settings to accelerator</p></body></html>"
+            ))
+        self.apply_btn.setText(_translate("MainWindow", "Apply"))
+        self.menu_File.setTitle(_translate("MainWindow", "&File"))
+        self.menuEdit.setTitle(_translate("MainWindow", "Tools"))
+        self.actionSave.setText(_translate("MainWindow", "Save"))
+        self.action_load_from_snp.setText(
+            _translate("MainWindow", "Load From Snapshot"))
+        self.action_load_from_snp.setShortcut(
+            _translate("MainWindow", "Ctrl+Shift+L"))
+        self.actionE_xit.setText(_translate("MainWindow", "E&xit"))
+        self.actionE_xit.setShortcut(_translate("MainWindow", "Ctrl+W"))
 
 
 from . import resources_rc
