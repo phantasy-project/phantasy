596f27d6cfb72ab68353094e5e073cad981780d8
Tong Zhang <zhangt@frib.msu.edu>
2019-01-24 13:20:07 -0500

    MNT/QS: Bump up version to 1.3.

--- phantasy/apps/quad_scan/__init__.py
+++ phantasy/apps/quad_scan/__init__.py
@@ -6,10 +6,10 @@ from PyQt5.QtWidgets import QApplication
 from .app import QuadScanWindow
 
 __authors__ = "Tong Zhang"
-__copyright__ = "(c) 2018, Facility for Rare Isotope beams," \
+__copyright__ = "(c) 2018-2019, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
-__version__ = '1.2'
+__version__ = '1.3'
 
 
 def run(cli=False):
