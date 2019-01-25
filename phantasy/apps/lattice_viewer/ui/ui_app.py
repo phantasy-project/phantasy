240be0546ec6b216508125e35dbd1613063f3764
Tong Zhang <zhangt@frib.msu.edu>
2019-01-24 22:33:36 -0500

    UI/LV: Update layout and resources.

--- phantasy/apps/lattice_viewer/ui/ui_app.py
+++ phantasy/apps/lattice_viewer/ui/ui_app.py
@@ -12,26 +12,23 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(927, 755)
+        MainWindow.resize(1200, 1000)
+        MainWindow.setStyleSheet("QLineEdit {\n"
+                                 "    border: 0.5px solid gray;\n"
+                                 "    padding: 1 5px;\n"
+                                 "    border-radius: 3px;\n"
+                                 "}")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setContentsMargins(8, 8, 8, 8)
+        self.gridLayout.setSpacing(5)
         self.gridLayout.setObjectName("gridLayout")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
-                                           QtWidgets.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.label.sizePolicy().hasHeightForWidth())
-        self.label.setSizePolicy(sizePolicy)
-        self.label.setStyleSheet("QLabel {\n"
-                                 "    font-weight: bold;\n"
-                                 "    font-size: 20pt;\n"
-                                 "}")
-        self.label.setAlignment(QtCore.Qt.AlignCenter)
-        self.label.setObjectName("label")
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName("verticalLayout")
         self.latticeWidget = LatticeWidget(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
                                            QtWidgets.QSizePolicy.Fixed)
@@ -41,18 +38,113 @@ class Ui_MainWindow(object):
             self.latticeWidget.sizePolicy().hasHeightForWidth())
         self.latticeWidget.setSizePolicy(sizePolicy)
         self.latticeWidget.setObjectName("latticeWidget")
-        self.gridLayout.addWidget(self.latticeWidget, 1, 0, 1, 1)
-        self.treeView = QtWidgets.QTreeView(self.centralwidget)
-        self.treeView.setStyleSheet("QTreeView {\n"
-                                    "    margin-left: 5px;\n"
-                                    "    margin-right: 5px;\n"
-                                    "}")
-        self.treeView.setAnimated(True)
+        self.verticalLayout.addWidget(self.latticeWidget)
+        self.gridLayout_3 = QtWidgets.QGridLayout()
+        self.gridLayout_3.setContentsMargins(-1, 0, -1, -1)
+        self.gridLayout_3.setVerticalSpacing(9)
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
+                                           QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout_3.addWidget(self.label_2, 0, 0, 1, 1)
+        self.elem_num_lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                           QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.elem_num_lineEdit.sizePolicy().hasHeightForWidth())
+        self.elem_num_lineEdit.setSizePolicy(sizePolicy)
+        self.elem_num_lineEdit.setReadOnly(True)
+        self.elem_num_lineEdit.setObjectName("elem_num_lineEdit")
+        self.gridLayout_3.addWidget(self.elem_num_lineEdit, 0, 1, 1, 1)
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setObjectName("label_3")
+        self.gridLayout_3.addWidget(self.label_3, 1, 0, 1, 1)
+        self.elem_types_lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                           QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.elem_types_lineEdit.sizePolicy().hasHeightForWidth())
+        self.elem_types_lineEdit.setSizePolicy(sizePolicy)
+        self.elem_types_lineEdit.setObjectName("elem_types_lineEdit")
+        self.gridLayout_3.addWidget(self.elem_types_lineEdit, 1, 1, 1, 1)
+        self.verticalLayout.addLayout(self.gridLayout_3)
+        self.horizontalLayout_3.addLayout(self.verticalLayout)
+        self.probeWidget = ProbeWidget(self.centralwidget)
+        self.probeWidget.setObjectName("probeWidget")
+        self.horizontalLayout_3.addWidget(self.probeWidget)
+        self.gridLayout.addLayout(self.horizontalLayout_3, 0, 0, 1, 1)
+        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox.setStyleSheet(
+            "QGroupBox {\n"
+            "    /*background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+            "                                      stop: 0 #E0E0E0, stop: 1 #E0E0E0);\n"
+            "   */\n"
+            "    border: 2px solid gray;\n"
+            "    border-radius: 5px;\n"
+            "    margin-top: 1.5ex; /* leave space at the top for the title */\n"
+            "    margin-bottom: 0.5ex;\n"
+            "}\n"
+            "\n"
+            "QGroupBox::title {\n"
+            "    subcontrol-origin: margin;\n"
+            "    subcontrol-position: top center; /* position at the top center */\n"
+            "    padding: 0 3px;\n"
+            "    /*background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+            "                                      stop: 0 #EDECEB, stop: 1 #FFFFFF);\n"
+            "    */\n"
+            "}")
+        self.groupBox.setObjectName("groupBox")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.groupBox)
+        self.gridLayout_2.setContentsMargins(5, 5, 5, 5)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.treeView = QtWidgets.QTreeView(self.groupBox)
         self.treeView.setObjectName("treeView")
-        self.gridLayout.addWidget(self.treeView, 2, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.treeView, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.groupBox, 1, 0, 1, 1)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setContentsMargins(-1, 0, -1, -1)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
+                                           QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.elem_sts_lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding,
+                                           QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.elem_sts_lineEdit.sizePolicy().hasHeightForWidth())
+        self.elem_sts_lineEdit.setSizePolicy(sizePolicy)
+        self.elem_sts_lineEdit.setReadOnly(True)
+        self.elem_sts_lineEdit.setObjectName("elem_sts_lineEdit")
+        self.horizontalLayout.addWidget(self.elem_sts_lineEdit)
+        self.gridLayout.addLayout(self.horizontalLayout, 2, 0, 1, 1)
+        self.latticeWidget.raise_()
+        self.groupBox.raise_()
+        self.probeWidget.raise_()
+        self.treeView.raise_()
+        self.elem_num_lineEdit.raise_()
+        self.label_2.raise_()
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 927, 34))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1200, 30))
         self.menubar.setObjectName("menubar")
         self.menu_File = QtWidgets.QMenu(self.menubar)
         self.menu_File.setObjectName("menu_File")
@@ -60,12 +152,36 @@ class Ui_MainWindow(object):
         self.menu_Help.setObjectName("menu_Help")
         MainWindow.setMenuBar(self.menubar)
         self.actionAbout = QtWidgets.QAction(MainWindow)
+        icon = QtGui.QIcon()
+        icon.addPixmap(
+            QtGui.QPixmap(":/icons/info.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.actionAbout.setIcon(icon)
         self.actionAbout.setObjectName("actionAbout")
         self.actionAbout_Qt = QtWidgets.QAction(MainWindow)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(
+            QtGui.QPixmap(":/icons/qt.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.actionAbout_Qt.setIcon(icon1)
         self.actionAbout_Qt.setObjectName("actionAbout_Qt")
         self.actionE_xit = QtWidgets.QAction(MainWindow)
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(
+            QtGui.QPixmap(":/icons/exit.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.actionE_xit.setIcon(icon2)
         self.actionE_xit.setObjectName("actionE_xit")
+        self.actionContents = QtWidgets.QAction(MainWindow)
+        icon3 = QtGui.QIcon()
+        icon3.addPixmap(
+            QtGui.QPixmap(":/icons/help.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.actionContents.setIcon(icon3)
+        self.actionContents.setObjectName("actionContents")
         self.menu_File.addAction(self.actionE_xit)
+        self.menu_Help.addAction(self.actionContents)
+        self.menu_Help.addSeparator()
         self.menu_Help.addAction(self.actionAbout)
         self.menu_Help.addAction(self.actionAbout_Qt)
         self.menubar.addAction(self.menu_File.menuAction())
@@ -82,7 +198,10 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "Lattice Viewer"))
+        self.label_2.setText(_translate("MainWindow", "Total Elements"))
+        self.label_3.setText(_translate("MainWindow", "Device Types"))
+        self.groupBox.setTitle(_translate("MainWindow", "Lattice"))
+        self.label.setText(_translate("MainWindow", "By Device Type"))
         self.menu_File.setTitle(_translate("MainWindow", "&File"))
         self.menu_Help.setTitle(_translate("MainWindow", "&Help"))
         self.actionAbout.setText(_translate("MainWindow", "About"))
@@ -90,9 +209,13 @@ class Ui_MainWindow(object):
         self.actionAbout_Qt.setText(_translate("MainWindow", "About Qt"))
         self.actionE_xit.setText(_translate("MainWindow", "E&xit"))
         self.actionE_xit.setShortcut(_translate("MainWindow", "Ctrl+W"))
+        self.actionContents.setText(_translate("MainWindow", "Contents"))
+        self.actionContents.setShortcut(_translate("MainWindow", "F1"))
 
 
 from phantasy_ui.widgets.latticewidget import LatticeWidget
+from phantasy_ui.widgets.probewidget import ProbeWidget
+from . import resources_rc
 
 if __name__ == "__main__":
     import sys
