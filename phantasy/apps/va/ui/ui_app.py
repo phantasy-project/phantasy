ccf3aed9f65e568c615263f41c3f2c2ca4a453f2
Tong Zhang <zhangt@frib.msu.edu>
2019-01-28 10:51:33 -0500

    UI/VA: Update UI and resources.

--- phantasy/apps/va/ui/ui_app.py
+++ phantasy/apps/va/ui/ui_app.py
@@ -12,7 +12,12 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(729, 413)
+        MainWindow.resize(729, 436)
+        icon = QtGui.QIcon()
+        icon.addPixmap(
+            QtGui.QPixmap(":/icons/frib_va.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
         MainWindow.setStyleSheet(
             "QGroupBox {\n"
             "    /*background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
@@ -37,58 +42,6 @@ class Ui_MainWindow(object):
         self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout_2.setContentsMargins(6, 6, 6, 6)
         self.gridLayout_2.setObjectName("gridLayout_2")
-        self.config_groupBox = QtWidgets.QGroupBox(self.centralwidget)
-        self.config_groupBox.setObjectName("config_groupBox")
-        self.gridLayout = QtWidgets.QGridLayout(self.config_groupBox)
-        self.gridLayout.setContentsMargins(5, 9, 5, 5)
-        self.gridLayout.setObjectName("gridLayout")
-        self.label = QtWidgets.QLabel(self.config_groupBox)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
-                                           QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.label.sizePolicy().hasHeightForWidth())
-        self.label.setSizePolicy(sizePolicy)
-        self.label.setObjectName("label")
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
-        self.mach_comboBox = QtWidgets.QComboBox(self.config_groupBox)
-        self.mach_comboBox.setObjectName("mach_comboBox")
-        self.gridLayout.addWidget(self.mach_comboBox, 0, 1, 1, 1)
-        self.label_2 = QtWidgets.QLabel(self.config_groupBox)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
-                                           QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.label_2.sizePolicy().hasHeightForWidth())
-        self.label_2.setSizePolicy(sizePolicy)
-        self.label_2.setObjectName("label_2")
-        self.gridLayout.addWidget(self.label_2, 0, 2, 1, 1)
-        self.engine_comboBox = QtWidgets.QComboBox(self.config_groupBox)
-        self.engine_comboBox.setObjectName("engine_comboBox")
-        self.engine_comboBox.addItem("")
-        self.engine_comboBox.addItem("")
-        self.gridLayout.addWidget(self.engine_comboBox, 0, 3, 1, 1)
-        self.label_6 = QtWidgets.QLabel(self.config_groupBox)
-        self.label_6.setObjectName("label_6")
-        self.gridLayout.addWidget(self.label_6, 1, 0, 1, 1)
-        self.segm_comboBox = QtWidgets.QComboBox(self.config_groupBox)
-        self.segm_comboBox.setObjectName("segm_comboBox")
-        self.gridLayout.addWidget(self.segm_comboBox, 1, 1, 1, 1)
-        self.label_5 = QtWidgets.QLabel(self.config_groupBox)
-        self.label_5.setObjectName("label_5")
-        self.gridLayout.addWidget(self.label_5, 1, 2, 1, 1)
-        self.prefix_comboBox = QtWidgets.QComboBox(self.config_groupBox)
-        self.prefix_comboBox.setEditable(True)
-        self.prefix_comboBox.setObjectName("prefix_comboBox")
-        self.prefix_comboBox.addItem("")
-        self.prefix_comboBox.addItem("")
-        self.gridLayout.addWidget(self.prefix_comboBox, 1, 3, 1, 1)
-        self.localonly_chkbox = QtWidgets.QCheckBox(self.config_groupBox)
-        self.localonly_chkbox.setObjectName("localonly_chkbox")
-        self.gridLayout.addWidget(self.localonly_chkbox, 2, 2, 1, 2)
-        self.gridLayout_2.addWidget(self.config_groupBox, 0, 0, 1, 1)
         self.control_groupBox = QtWidgets.QGroupBox(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
                                            QtWidgets.QSizePolicy.Preferred)
@@ -246,7 +199,66 @@ class Ui_MainWindow(object):
         self.horizontalLayout.addItem(spacerItem1)
         self.verticalLayout_2.addLayout(self.horizontalLayout)
         self.gridLayout_2.addWidget(self.control_groupBox, 1, 0, 1, 1)
+        self.config_groupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.config_groupBox.setObjectName("config_groupBox")
+        self.gridLayout = QtWidgets.QGridLayout(self.config_groupBox)
+        self.gridLayout.setContentsMargins(5, 9, 5, 5)
+        self.gridLayout.setObjectName("gridLayout")
+        self.label = QtWidgets.QLabel(self.config_groupBox)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
+                                           QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.mach_comboBox = QtWidgets.QComboBox(self.config_groupBox)
+        self.mach_comboBox.setObjectName("mach_comboBox")
+        self.gridLayout.addWidget(self.mach_comboBox, 0, 1, 1, 1)
+        self.label_2 = QtWidgets.QLabel(self.config_groupBox)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
+                                           QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 0, 2, 1, 1)
+        self.engine_comboBox = QtWidgets.QComboBox(self.config_groupBox)
+        self.engine_comboBox.setObjectName("engine_comboBox")
+        self.engine_comboBox.addItem("")
+        self.engine_comboBox.addItem("")
+        self.gridLayout.addWidget(self.engine_comboBox, 0, 3, 1, 1)
+        self.label_6 = QtWidgets.QLabel(self.config_groupBox)
+        self.label_6.setObjectName("label_6")
+        self.gridLayout.addWidget(self.label_6, 1, 0, 1, 1)
+        self.segm_comboBox = QtWidgets.QComboBox(self.config_groupBox)
+        self.segm_comboBox.setObjectName("segm_comboBox")
+        self.gridLayout.addWidget(self.segm_comboBox, 1, 1, 1, 1)
+        self.label_5 = QtWidgets.QLabel(self.config_groupBox)
+        self.label_5.setObjectName("label_5")
+        self.gridLayout.addWidget(self.label_5, 1, 2, 1, 1)
+        self.prefix_comboBox = QtWidgets.QComboBox(self.config_groupBox)
+        self.prefix_comboBox.setEditable(True)
+        self.prefix_comboBox.setObjectName("prefix_comboBox")
+        self.prefix_comboBox.addItem("")
+        self.prefix_comboBox.addItem("")
+        self.gridLayout.addWidget(self.prefix_comboBox, 1, 3, 1, 1)
+        self.localonly_chkbox = QtWidgets.QCheckBox(self.config_groupBox)
+        self.localonly_chkbox.setObjectName("localonly_chkbox")
+        self.gridLayout.addWidget(self.localonly_chkbox, 2, 2, 1, 2)
+        self.gridLayout_2.addWidget(self.config_groupBox, 0, 0, 1, 1)
         self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                           QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.groupBox.sizePolicy().hasHeightForWidth())
+        self.groupBox.setSizePolicy(sizePolicy)
         self.groupBox.setObjectName("groupBox")
         self.horizontalLayout_4 = QtWidgets.QHBoxLayout(self.groupBox)
         self.horizontalLayout_4.setContentsMargins(5, 5, 5, 5)
@@ -255,7 +267,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.va_status_label = QtWidgets.QLabel(self.groupBox)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed,
-                                           QtWidgets.QSizePolicy.Preferred)
+                                           QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(
@@ -283,11 +295,18 @@ class Ui_MainWindow(object):
                                             QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout_2.addItem(spacerItem2)
         self.label_3 = QtWidgets.QLabel(self.groupBox)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                           QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.label_3.sizePolicy().hasHeightForWidth())
+        self.label_3.setSizePolicy(sizePolicy)
         self.label_3.setObjectName("label_3")
         self.horizontalLayout_2.addWidget(self.label_3)
         self.uptime_label = QtWidgets.QLabel(self.groupBox)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
-                                           QtWidgets.QSizePolicy.Preferred)
+                                           QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(
@@ -303,9 +322,13 @@ class Ui_MainWindow(object):
         self.horizontalLayout_2.addWidget(self.uptime_label)
         self.horizontalLayout_4.addLayout(self.horizontalLayout_2)
         self.gridLayout_2.addWidget(self.groupBox, 2, 0, 1, 1)
+        spacerItem3 = QtWidgets.QSpacerItem(20, 40,
+                                            QtWidgets.QSizePolicy.Minimum,
+                                            QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout_2.addItem(spacerItem3, 3, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 729, 29))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 729, 30))
         self.menubar.setObjectName("menubar")
         self.menu_Help = QtWidgets.QMenu(self.menubar)
         self.menu_Help.setObjectName("menu_Help")
@@ -316,40 +339,60 @@ class Ui_MainWindow(object):
         self.toolBar.setObjectName("toolBar")
         MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
         self.actionAbout = QtWidgets.QAction(MainWindow)
-        icon = QtGui.QIcon()
-        icon.addPixmap(
-            QtGui.QPixmap(":/icons/info.png"), QtGui.QIcon.Normal,
-            QtGui.QIcon.Off)
-        self.actionAbout.setIcon(icon)
-        self.actionAbout.setObjectName("actionAbout")
-        self.actionAbout_Qt = QtWidgets.QAction(MainWindow)
         icon1 = QtGui.QIcon()
         icon1.addPixmap(
-            QtGui.QPixmap(":/icons/qt.png"), QtGui.QIcon.Normal,
+            QtGui.QPixmap(":/icons/info.png"), QtGui.QIcon.Normal,
             QtGui.QIcon.Off)
-        self.actionAbout_Qt.setIcon(icon1)
-        self.actionAbout_Qt.setObjectName("actionAbout_Qt")
-        self.actionContents = QtWidgets.QAction(MainWindow)
+        self.actionAbout.setIcon(icon1)
+        self.actionAbout.setObjectName("actionAbout")
+        self.actionAbout_Qt = QtWidgets.QAction(MainWindow)
         icon2 = QtGui.QIcon()
         icon2.addPixmap(
-            QtGui.QPixmap(":/icons/help.png"), QtGui.QIcon.Normal,
+            QtGui.QPixmap(":/icons/qt.png"), QtGui.QIcon.Normal,
             QtGui.QIcon.Off)
-        self.actionContents.setIcon(icon2)
-        self.actionContents.setObjectName("actionContents")
-        self.actionE_xit = QtWidgets.QAction(MainWindow)
+        self.actionAbout_Qt.setIcon(icon2)
+        self.actionAbout_Qt.setObjectName("actionAbout_Qt")
+        self.actionContents = QtWidgets.QAction(MainWindow)
         icon3 = QtGui.QIcon()
         icon3.addPixmap(
+            QtGui.QPixmap(":/icons/help.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.actionContents.setIcon(icon3)
+        self.actionContents.setObjectName("actionContents")
+        self.actionE_xit = QtWidgets.QAction(MainWindow)
+        icon4 = QtGui.QIcon()
+        icon4.addPixmap(
             QtGui.QPixmap(":/icons/exit.png"), QtGui.QIcon.Normal,
             QtGui.QIcon.Off)
-        self.actionE_xit.setIcon(icon3)
+        self.actionE_xit.setIcon(icon4)
         self.actionE_xit.setObjectName("actionE_xit")
         self.nb_tool = QtWidgets.QAction(MainWindow)
+        icon5 = QtGui.QIcon()
+        icon5.addPixmap(
+            QtGui.QPixmap(":/icons/notebook_run.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.nb_tool.setIcon(icon5)
         self.nb_tool.setObjectName("nb_tool")
         self.va_info_tool = QtWidgets.QAction(MainWindow)
+        icon6 = QtGui.QIcon()
+        icon6.addPixmap(
+            QtGui.QPixmap(":/icons/system-task.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.va_info_tool.setIcon(icon6)
         self.va_info_tool.setObjectName("va_info_tool")
         self.va_run_tool = QtWidgets.QAction(MainWindow)
+        icon7 = QtGui.QIcon()
+        icon7.addPixmap(
+            QtGui.QPixmap(":/icons/play.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.va_run_tool.setIcon(icon7)
         self.va_run_tool.setObjectName("va_run_tool")
         self.va_stop_tool = QtWidgets.QAction(MainWindow)
+        icon8 = QtGui.QIcon()
+        icon8.addPixmap(
+            QtGui.QPixmap(":/icons/stop.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.va_stop_tool.setIcon(icon8)
         self.va_stop_tool.setObjectName("va_stop_tool")
         self.menu_Help.addAction(self.actionContents)
         self.menu_Help.addSeparator()
@@ -391,6 +434,14 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.control_groupBox.setTitle(_translate("MainWindow", "Control"))
+        self.label_4.setText(_translate("MainWindow", "Noise Level"))
+        self.noise_label.setText(_translate("MainWindow", "0.0%"))
+        self.label_7.setText(_translate("MainWindow", "MPS Status"))
+        self.mps_fault_radiobtn.setText(_translate("MainWindow", "Fault"))
+        self.mps_disable_radiobtn.setText(_translate("MainWindow", "Disable"))
+        self.mps_monitor_radiobtn.setText(_translate("MainWindow", "Monitor"))
+        self.mps_enable_radiobtn.setText(_translate("MainWindow", "Enable"))
         self.config_groupBox.setTitle(
             _translate("MainWindow", "Configuration"))
         self.label.setText(_translate("MainWindow", "Machine"))
@@ -403,14 +454,6 @@ class Ui_MainWindow(object):
         self.prefix_comboBox.setItemText(1, _translate("MainWindow", "VA"))
         self.localonly_chkbox.setText(
             _translate("MainWindow", "Limit CA Local Only"))
-        self.control_groupBox.setTitle(_translate("MainWindow", "Control"))
-        self.label_4.setText(_translate("MainWindow", "Noise Level"))
-        self.noise_label.setText(_translate("MainWindow", "0.0%"))
-        self.label_7.setText(_translate("MainWindow", "MPS Status"))
-        self.mps_fault_radiobtn.setText(_translate("MainWindow", "Fault"))
-        self.mps_disable_radiobtn.setText(_translate("MainWindow", "Disable"))
-        self.mps_monitor_radiobtn.setText(_translate("MainWindow", "Monitor"))
-        self.mps_enable_radiobtn.setText(_translate("MainWindow", "Enable"))
         self.groupBox.setTitle(_translate("MainWindow", "Status"))
         self.va_status_label.setText(_translate("MainWindow", "Stopped"))
         self.label_3.setText(_translate("MainWindow", "Uptime"))
