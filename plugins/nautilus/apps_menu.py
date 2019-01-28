2841932d7814f45b50ae74a9bb277b0509b89072
Tong Zhang <zhangt@frib.msu.edu>
2019-01-27 21:25:45 -0500

    ENH: Create plugin for launching phyapps from context menu.

---
+++ plugins/nautilus/apps_menu.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from gi.repository import Nautilus as Explorer
+from gi.repository import GObject
+
+import os
+from subprocess import Popen, PIPE
+
+LOG_PATH = "/tmp/phantasy_logs/"
+
+if not os.path.exists(LOG_PATH):
+    os.makedirs(LOG_PATH)
+
+
+class ColumnExtension(GObject.GObject, Explorer.MenuProvider):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+
+    def get_background_items(self, window, file):
+        top_mitem = Explorer.MenuItem(
+                name="AppsMenu::Physics Apps",
+                label="Physics Apps",
+                tip="Global Launcher for Physics Applications",
+                icon="phantasy-al")
+
+        top_mitem.connect('activate', self.start_app_launcher, file)
+
+        return top_mitem,
+
+    def start_app_launcher(self, menu, file):
+        cmd = "app_launcher"
+        fn = datetime.now().strftime('%Y%m%d_%H%M%S')
+        logfile = os.path.join(LOG_PATH, fn)
+        with open(logfile, 'w') as fid:
+            p = Popen(cmd, shell=True,
+                      stdin=PIPE, stdout=fid, stderr=fid,
+                      close_fds=True)
