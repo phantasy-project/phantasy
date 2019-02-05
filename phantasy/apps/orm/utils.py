1aac79c309270c7e5b92f221f2455674eb2d95ca
Tong Zhang <zhangt@frib.msu.edu>
2019-02-05 11:06:56 -0500

    ENH/ORM: Unify the thread worker for measurement and correction.

--- phantasy/apps/orm/utils.py
+++ phantasy/apps/orm/utils.py
@@ -15,29 +15,33 @@ except:
 from phantasy import get_orm
 
 
-class OrmRunner(QObject):
-    #
+class OrmWorker(QObject):
     started = pyqtSignal()
-    #
     finished = pyqtSignal()
-    # ORM matrix, np.ndarray
+
+    # orm or str
     resultsReady = pyqtSignal(QVariant)
+
     # percentage, name of corrector
     update_progress = pyqtSignal(float, 'QString')
-    """Worker for ORM measurement.
 
-    Parameters
-    ----------
-    """
-    def __init__(self, params):
+    def __init__(self, params, mode="measure"):
+        # mode: 'measure' or 'apply'
         super(self.__class__, self).__init__()
-        (bpms, cors), (source, srange, cor_field, xoy, wait) = params
+        self._mode = mode
+        if mode == 'measure':
+            (bpms, cors), (source, srange, cor_field, xoy, wait) = params
+            self._source = source
+            self._cor_field = cor_field
+            self._srange = srange
+        else:
+            (lat,), (bpms, cors), (xoy, dfac, niter, wait) = params
+            self._lat = lat
+            self._dfac = dfac
+            self._niter = niter
 
         self._bpms = bpms
         self._cors = cors
-        self._source = source
-        self._srange = srange
-        self._cor_field = cor_field
         self._xoy = xoy
         self._wait= wait
 
@@ -45,11 +49,18 @@ class OrmRunner(QObject):
         self.started.emit()
         q = Queue(0)
         self.message_receiver(q)
-        m = get_orm(self._cors, self._bpms,
-                    source=self._source, scan=self._srange,
-                    cor_field=self._cor_field, xoy=self._xoy,
-                    wait=self._wait,
-                    msg_queue=q)
+        if self._mode == 'measure':
+            m = get_orm(self._cors, self._bpms,
+                        source=self._source, scan=self._srange,
+                        cor_field=self._cor_field, xoy=self._xoy,
+                        wait=self._wait,
+                        msg_queue=q)
+        else:
+            m = self._lat.correct_orbit(self._cors, self._bpms,
+                                       xoy=self._xoy,
+                                       damping_factor=self._dfac,
+                                       iteration=self._niter, wait=self._wait,
+                                       msg_queue=q)
         q.join()
         self.resultsReady.emit(m)
         self.finished.emit()
@@ -66,58 +77,3 @@ class OrmRunner(QObject):
         receiver = Thread(target=_receiver, args=(q,))
         receiver.setDaemon(True)
         receiver.start()
-
-
-class OrmConsumer(QObject):
-    #
-    started = pyqtSignal()
-    #
-    finished = pyqtSignal()
-    #
-    resultsReady = pyqtSignal()
-    #
-    update_progress = pyqtSignal(float, 'QString')
-    """Worker for orbit correction with ORM.
-
-    Parameters
-    ----------
-    """
-    def __init__(self, params):
-        super(self.__class__, self).__init__()
-        (lat,), (bpms, cors), (xoy, dfac, niter, wait) = params
-
-        self._lat = lat
-        self._bpms = bpms
-        self._cors = cors
-        self._xoy = xoy
-        self._wait= wait
-        self._dfac = dfac
-        self._niter = niter
-
-    def run(self):
-        self.started.emit()
-        q = Queue(0)
-        self.message_receiver(q)
-        self._lat.correct_orbit(self._cors, self._bpms,
-                                xoy=self._xoy,
-                                damping_factor=self._dfac,
-                                iteration=self._niter, wait=self._wait,
-                                msg_queue=q)
-        q.join()
-        self.resultsReady.emit()
-        self.finished.emit()
-
-    def message_receiver(self, q):
-        """Message receiver and processor from the message queue *q*.
-        """
-        def _receiver(q):
-            while True:
-                per, msg = q.get()
-                self.update_progress.emit(per, msg)
-                q.task_done()
-
-        receiver = Thread(target=_receiver, args=(q,))
-        receiver.setDaemon(True)
-        receiver.start()
-
-
