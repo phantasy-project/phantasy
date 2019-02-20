23106c16d772e0fc91fec446b2683da301c2c564
Tong Zhang <zhangt@frib.msu.edu>
2019-02-19 21:50:23 -0500

    MNT: Add get_orm_for_one_corrector to 'phantasy' namespace.

--- phantasy/library/physics/__init__.py
+++ phantasy/library/physics/__init__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 #
-# Physics related modules 
+# Physics related modules
 #
 
 from .geometry import Point
 from .geometry import Line
 from .particles import Distribution
 from .orm import get_orm
+from .orm import get_orm_for_one_corrector
 from .orm import get_orbit
 from .orm import inverse_matrix
 from .orm import get_correctors_settings
@@ -16,4 +17,5 @@ from .orm import get_index_grid
 __all__ = ['Point', 'Line', 'Distribution',
            'get_orm', 'get_orbit', 'inverse_matrix',
            'get_correctors_settings',
-           'get_index_grid']
+           'get_index_grid',
+           'get_orm_for_one_corrector',]
