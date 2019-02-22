c37ebef5072ff4c4673753e02ddd6a2f36624e6d
Tong Zhang <zhangt@frib.msu.edu>
2019-02-22 13:32:25 -0500

    ENH/ORM: Resize, keyshort changes to SettingsView widget.

--- phantasy/apps/orm/app_settings_view.py
+++ phantasy/apps/orm/app_settings_view.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
+from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QDialog
 from PyQt5.QtCore import QSize
+from PyQt5.QtCore import Qt
 
 from .ui.ui_settings_view import Ui_Dialog
 from .utils import SettingsModel
@@ -21,9 +22,34 @@ class SettingsView(QDialog, Ui_Dialog):
         self.v = self.treeView
 
         model = SettingsModel(self.v, self.data)
+        model.view_size.connect(self.on_resize)
         model.set_model()
+        self._pvs = model._pvs
 
-        self.adjustSize()
+    def on_resize(self, w, h):
+        rect = self.geometry()
+        w0, h0 = rect.width(), rect.height()
+        if h0 < h:
+            h = h0
+        self.resize(w * 1.05, h * 1.05)
 
-    def sizeHint(self):
-        return QSize(1600, 900)
+    def closeEvent(self, e):
+        for pv in self._pvs:
+            pv.clear_callbacks()
+        QDialog.closeEvent(self, e)
+
+    @pyqtSlot()
+    def on_click_ok(self):
+        self.close()
+        self.setResult(QDialog.Accepted)
+
+    @pyqtSlot()
+    def on_click_cancel(self):
+        self.close()
+        self.setResult(QDialog.Rejected)
+
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Escape:
+            self.close()
+        else:
+            QDialog.keyPressEvent(self, e)
