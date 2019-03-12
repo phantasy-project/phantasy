6715de1c640e287d3421ecefe35efd4587c6949d
Tong Zhang <zhangt@frib.msu.edu>
2019-03-12 11:20:25 -0400

    ENH/CV: ElementSelectDialog supports multi-element selection.
    - mode = 'extra'

--- phantasy/apps/correlation_visualizer/app_elem_select.py
+++ phantasy/apps/correlation_visualizer/app_elem_select.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import re
-from collections import OrderedDict
+from collections import deque
 
 from PyQt5.QtWidgets import QDialog
 from PyQt5.QtWidgets import QMessageBox
@@ -19,11 +19,16 @@ from phantasy.apps.correlation_visualizer.utils import PVElementReadonly
 from phantasy.apps.correlation_visualizer.utils import milli_sleep1
 from phantasy.apps.correlation_visualizer.utils import delayed_check_pv_status
 
+# max number of elements for selection
+NMAX = 100
+
 
 class ElementSelectDialog(QDialog, Ui_Dialog):
 
     def __init__(self, parent=None, mode='alter', mp=None):
-        # mode: 'alter' or 'monitor'
+        # mode: 'alter' or 'monitor', 'extra'
+        # 'alter' and 'monitor': single selection
+        # 'extra': multiple selection
         # monitor: only show readback PV
         super(ElementSelectDialog, self).__init__()
         self.parent = parent
@@ -54,10 +59,6 @@ class ElementSelectDialog(QDialog, Ui_Dialog):
         # PV mode: [set]/read PVs
         self.pv_set_lineEdit.returnPressed.connect(self.set_elem)
         self.pv_read_lineEdit.returnPressed.connect(self.set_elem)
-        self.sel_elem = None  # selected element obj
-
-        # Element mode
-        self.sel_field = None # seleted field name
 
         # validate flag
         self._already_validated = False
@@ -70,8 +71,19 @@ class ElementSelectDialog(QDialog, Ui_Dialog):
         self.pv_mode_radiobtn.toggled.emit(True)
         self.elem_mode_radiobtn.toggled.emit(False)
 
-        # Element mode, selected elements
-        self.__element = OrderedDict()
+        # element selection
+        self.__init_element_selection(self.mode)
+
+    def __init_element_selection(self, mode):
+        # initialize element selection
+        if mode != 'extra':
+            n = 1
+        else:
+            n = NMAX
+        self.__elements = deque([], n)
+        self.sel_elem = deque([], n)          # selected CaField objs
+        self.sel_elem_display = deque([], n)  # Element objs
+        self.sel_field = deque([], n)         # seleted field names
 
     @pyqtSlot()
     def on_click_ok(self):
@@ -88,61 +100,54 @@ class ElementSelectDialog(QDialog, Ui_Dialog):
         self.close()
         self.setResult(QDialog.Rejected)
 
+    def _is_elements_all_connected(self, elem_list):
+        # return True if all elements in *elem_list* are connected.
+        for elem in elem_list:
+            if not elem.connected:
+                QMessageBox.warning(self, "",
+                    "{} cannot be reached.".format(elem.ename),
+                    QMessageBox.Ok)
+                return False
+        return True
+
     @pyqtSlot()
     def on_validate(self):
         """To validate the selected element CA connection.
         """
         if self.pv_mode_radiobtn.isChecked():
-            #if self.sel_elem is None:
+            # generic
             self.set_elem()
-            try:
-                if self.sel_elem.connected:
-                    QMessageBox.information(self, "",
-                            "Device is connected",
-                            QMessageBox.Ok)
-                    is_valid = True
-                    self._already_validated = True
-                else:
-                    QMessageBox.warning(self, "",
-                            "Device is not connected",
-                            QMessageBox.Ok)
-                    is_valid = False
-            except:
-                QMessageBox.critical(self, "",
-                        "Validating failed",
-                        QMessageBox.Ok)
-                is_valid = False
         else:
             # high-level element
             self.set_elem_high_level()
-            try:
-                if self.sel_elem.connected:
-                    QMessageBox.information(self, "",
-                            "Device is connected",
-                            QMessageBox.Ok)
-                    is_valid = True
-                    self._already_validated = True
-                else:
-                    QMessageBox.warning(self, "",
-                            "Device is not connected",
-                            QMessageBox.Ok)
-                    is_valid = False
-            except:
-                QMessageBox.critical(self, "",
-                        "Validating failed",
+        try:
+            if self._is_elements_all_connected(self.sel_elem):
+                QMessageBox.information(self, "",
+                        "Devices are all connected.",
+                        QMessageBox.Ok)
+                is_valid = True
+                self._already_validated = True
+            else:
+                QMessageBox.warning(self, "",
+                        "Devices are not all connected.",
                         QMessageBox.Ok)
                 is_valid = False
+        except:
+            QMessageBox.critical(self, "",
+                    "Validating failed",
+                    QMessageBox.Ok)
+            is_valid = False
 
         return is_valid
 
     def set_elem_high_level(self):
-        """Build Element from selected high-level element.
+        """Build selected high-level element(s).
         """
-        for k, v in self.__element.items():
-            # k: (ename, fname), v: elem
-            self.sel_elem = v.get_field(k[1])
-            self.sel_field = self.sel_elem.name  # field name
-            self.sel_elem_display = v            # elem
+        for ename, fname, elem in self.__elements:
+            print(ename, fname, elem.name)
+            self.sel_elem.append(elem.get_field(fname))
+            self.sel_field.append(fname)
+            self.sel_elem_display.append(elem )
 
     @pyqtSlot()
     def set_elem(self):
@@ -153,18 +158,20 @@ class ElementSelectDialog(QDialog, Ui_Dialog):
             getPV_str = self.pv_read_lineEdit.text()
             if putPV_str == '' or getPV_str == '':
                 return
-            self.sel_elem = PVElement(putPV_str, getPV_str)
-        elif self.mode == 'monitor':
+            sel_elem = PVElement(putPV_str, getPV_str)
+        else:  # 'monitor' and 'extra'
             getPV_str = self.pv_read_lineEdit.text()
             if getPV_str == '':
                 return
-            self.sel_elem = PVElementReadonly(getPV_str)
+            sel_elem = PVElementReadonly(getPV_str)
 
-        self.sel_elem_display = self.sel_elem
-        self.sel_field = None
+        self.sel_elem.append(sel_elem)
+        self.sel_elem_display.append(sel_elem)
+        self.sel_field.append(None)
 
         milli_sleep1(2000)
-        delayed_check_pv_status(self, self.sel_elem, 100)
+        for pvelem in self.sel_elem:
+            delayed_check_pv_status(self, pvelem, 100)
 
     @pyqtSlot()
     def on_copy_setpv(self):
@@ -184,14 +191,30 @@ class ElementSelectDialog(QDialog, Ui_Dialog):
 
     @pyqtSlot('QString', 'QString', QVariant)
     def on_element_selected(self, ename, fname, elem):
-        ekey = (ename, fname)
-        if ekey not in self.__element:
-            self.__element.update([(ekey, elem)])
-        else:
-            self.__element.pop(ekey)
+        self.__elements.append((ename, fname, elem))
+        # debug
+        print(self.__elements)
+        #
 
     def sizeHint(self):
         return QSize(600, 400)
+
+    def on_select_element(self, index):
+        # slot: dbclick to select
+        v = self.elem_treeView
+        m = v.model()
+        row = index.row()
+        item = m.item(row, m.i_name)
+        if item.checkState() == Qt.Checked:
+            item.setCheckState(Qt.Unchecked)
+        else:
+            item.setCheckState(Qt.Checked)
+        fld_widget = v.indexWidget(m.index(row, m.i_field))
+        fname = fld_widget.currentText()
+
+        msg = "{}/{} now is checked? {}".format(
+                item.text(), fname, item.checkState()==Qt.Checked)
+        print(msg)
 
 
 if __name__ == '__main__':
