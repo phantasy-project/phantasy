0ba26bd115c98e5930c9bf88cccf6c9a4e637c70
Tong Zhang <zhangt@frib.msu.edu>
2019-03-19 11:08:29 -0400

    ENH/DV: Refactor with ElementListModel and LatticeWidget.

--- phantasy/apps/diag_viewer/app.py
+++ phantasy/apps/diag_viewer/app.py
@@ -9,9 +9,15 @@ from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QMessageBox
 
 from phantasy_ui.templates import BaseAppForm
+from phantasy_ui.widgets.latticewidget import LatticeWidget
 
 from .ui.ui_app import Ui_MainWindow
-from .utils import DeviceLoader
+
+from collections import OrderedDict
+from phantasy.apps.trajectory_viewer.app_elem_selection import ElementSelectionWidget
+from .utils import ElementListModelDV as ElementListModel
+
+DTYPE_LIST = ("BCM", )
 
 
 class DiagViewerWindow(BaseAppForm, Ui_MainWindow):
@@ -20,6 +26,8 @@ class DiagViewerWindow(BaseAppForm, Ui_MainWindow):
     data_updated = pyqtSignal(QVariant, QVariant, QVariant)
     # init
     data_initialized = pyqtSignal(QVariant, QVariant, QVariant)
+    ## selected devices and fields, k: ename, v: list of fields
+    #devicesChanged = pyqtSignal(dict)
 
     def __init__(self, version):
         super(DiagViewerWindow, self).__init__()
@@ -52,17 +60,16 @@ class DiagViewerWindow(BaseAppForm, Ui_MainWindow):
         self._post_init()
 
     def _post_init(self,):
+        # lattice load window
+        self._lattice_load_window = None
+        # elem selection widget
+        self._elem_sel_widget = None
         #
-        self._segs_chkbox = (self.lebt_chkbox, self.mebt_chkbox,
-                             self.mebt2fs1a_chkbox, self.mebt2fs1b_chkbox)
-        self._dtype = self.device_type_cbb.currentText()
-        self.machine_cbb.currentTextChanged.emit(self.machine_cbb.currentText())
-
-        #
-        self.load_pb.setVisible(False)
-        # names and elements of loaded devices
-        self._names = None
-        self._elems = None
+        # list of field names of selected elements
+        self.__mp = None
+        self._field_list = []
+        # list of selected elems
+        self._elems_list = []
 
         # DAQ
         self.daq_timer = QTimer()
@@ -84,87 +91,43 @@ class DiagViewerWindow(BaseAppForm, Ui_MainWindow):
         # annote
         self._show_annote = False
 
+        # device selection
+        self.choose_elems_btn.clicked.connect(self.on_list_devices)
+        self.select_all_elems_btn.clicked.connect(self.on_select_all_elems)
+        self.inverse_selection_btn.clicked.connect(self.on_inverse_current_elem_selection)
+
+        # field cbb
+        self.field_cbb.currentTextChanged.connect(self.on_elem_field_changed)
+
+    @pyqtSlot()
+    def on_select_all_elems(self):
+        try:
+            model = self.devices_treeView.model()
+            model.select_all_items()
+        except AttributeError:
+            QMessageBox.warning(self, "Element Selection",
+                    "Selection error, Choose elements first.",
+                    QMessageBox.Ok)
+
+    @pyqtSlot()
+    def on_inverse_current_elem_selection(self):
+        try:
+            model = self.devices_treeView.model()
+            model.inverse_current_selection()
+        except AttributeError:
+            QMessageBox.warning(self, "Element Selection",
+                    "Selection error, Choose elements first.",
+                    QMessageBox.Ok)
+
     @pyqtSlot(float)
     def update_daqfreq(self, f):
         self._daqfreq = f
-
-    @pyqtSlot('QString')
-    def on_device_type_changed(self, s):
-        self._dtype = s
-
-    @pyqtSlot()
-    def on_load_devices(self):
-        """Load devices based on selected type.
-        """
-        if self._mach == "FRIB":
-            self._segs = [o.text() for o in self._segs_chkbox if o.isChecked()]
-        else: # VA
-            self._segs = ["LINAC"]
-        if self._segs == []:
-            QMessageBox.warning(self, "Device Loading Warning",
-                    "Please select segments.", QMessageBox.Ok)
-            return
-
-        print("Load device of type: {}".format(self._dtype))
-        self.load_pb.setVisible(True)
-        self.load_status_lbl.setText("Loading...")
-        self.thread = QThread()
-        self.loader = DeviceLoader(self._mach, self._segs, self._dtype)
-        self.loader.moveToThread(self.thread)
-        self.loader.results_ready.connect(self.on_results_ready)
-        self.loader.finished.connect(self.load_complete)
-        self.loader.finished.connect(self.thread.quit)
-        self.loader.finished.connect(self.loader.deleteLater)
-        self.thread.finished.connect(self.thread.deleteLater)
-        self.thread.started.connect(self.loader.run)
-        self.thread.start()
-        self.load_btn.setEnabled(False)
-
-    @pyqtSlot(bool)
-    def load_complete(self, is_load_success):
-        """Lattice loading completed.
-        """
-        self.load_pb.setVisible(False)
-        if is_load_success:
-            info = 'Loaded ({}) {}s from {}'.format(len(self._names),
-                                                    self._dtype, self._mach)
-        else:
-            info = 'Failed to load {} from {}'.format(self._dtype, self._mach)
-        self.load_status_lbl.setText(info)
-        self.load_btn.setEnabled(True)
-
-    @pyqtSlot(QVariant, QVariant)
-    def on_results_ready(self, names, elems):
-        """Results are ready.
-        """
-        if names is None:
-            QMessageBox.warning(self, "Device Loading Warning",
-                    "Failed to load devices.", QMessageBox.Ok)
-            return
-        self._names = names
-        self._elems = elems
-        self.__update_fields()
-
-    def __update_fields(self):
-        # update monitor fields cbb.
-        if self._elems:
-            self.field_cbb.clear()
-            self.field_cbb.addItems(self._elems[0].fields)
-        else:
-            QMessageBox.warning(self, "Device Loading Warning",
-                    "Cannot find {} devices.".format(self._dtype))
-
-    @pyqtSlot('QString')
-    def on_machine_changed(self, s):
-        # machine is changed
-        [o.setEnabled(s=="FRIB") for o in self._segs_chkbox]
-        self._mach = s
 
     @pyqtSlot()
     def on_daq_start(self):
         """Start DAQ.
         """
-        if self._names is None:
+        if self._elems_list == [] :
             QMessageBox.warning(self, "DAQ Warning",
                     "Cannot find loaded devices.", QMessageBox.Ok)
             return
@@ -188,10 +151,10 @@ class DiagViewerWindow(BaseAppForm, Ui_MainWindow):
         self.annote_height_chkbox.toggled.emit(self._show_annote)
 
     def __refresh_data(self):
-        field = self.field_cbb.currentText()
-        h = [getattr(e, field) for e in self._elems]
+        h = [getattr(e, f) for e, f in zip(self._elems_list, self._field_list)]
+
         if self._xdata_gauge == 'pos':
-            s = [e.sb for e in self._elems]
+            s = [e.sb for e in self._elems_list]
         else: # ID as x
             s = list(range(len(h)))
         herr = [0] * len(h)
@@ -222,12 +185,14 @@ class DiagViewerWindow(BaseAppForm, Ui_MainWindow):
             self.id_as_x_rbtn.setEnabled(True)
             self.pos_as_x_rbtn.setEnabled(True)
             self.stop_btn.setEnabled(False)
+            self.devices_treeView.setEnabled(True)
         else:
             self.reset_figure_btn.setEnabled(False)
             self.start_btn.setEnabled(False)
             self.id_as_x_rbtn.setEnabled(False)
             self.pos_as_x_rbtn.setEnabled(False)
             self.stop_btn.setEnabled(True)
+            self.devices_treeView.setEnabled(False)
 
     @pyqtSlot(bool)
     def on_annote_height(self, f):
@@ -242,3 +207,74 @@ class DiagViewerWindow(BaseAppForm, Ui_MainWindow):
             # hide annotes
             [i.set_visible(False) for i in o._all_annotes]
         o.update_figure()
+
+    @pyqtSlot()
+    def onLoadLatticeAction(self):
+        """Load lattice.
+        """
+        if self._lattice_load_window is None:
+            self._lattice_load_window = LatticeWidget()
+        self._lattice_load_window.show()
+        self._lattice_load_window.latticeChanged.connect(self.update_lattice)
+        # reset element selection widgets
+        self._elem_sel_widget = None
+
+    @pyqtSlot(QVariant)
+    def update_lattice(self, o):
+        self.__mp = o
+
+    @pyqtSlot()
+    def on_list_devices(self):
+        if self.__mp is None:
+            QMessageBox.warning(self, "Device Selection",
+                    "Cannot find loaded lattice, try to load first, either by clicking Tools > Load Lattice or Ctrl+Shift+L.",
+                    QMessageBox.Ok)
+            return
+        w = self._elem_sel_widget = ElementSelectionWidget(self,
+                self.__mp, dtypes=DTYPE_LIST)
+        w.elementsSelected.connect(self.on_update_elems)
+        w.show()
+
+    @pyqtSlot(list)
+    def on_update_elems(self, enames):
+        """Selected element names list updated.
+        """
+        tv = self.devices_treeView
+        model = ElementListModel(tv, self.__mp, enames)
+        # list of fields of selected element type
+        model.fieldsSelected.connect(self.on_selected_fields_updated)
+        model.set_model()
+
+        m = tv.model()
+        m.elementSelected.connect(self.on_elem_selection_updated)
+        model.select_all_items()
+
+    @pyqtSlot(OrderedDict)
+    def on_elem_selection_updated(self, d):
+        print("selected elems:", d)
+        self._field_list = [f[0] for _, f in d.items()]
+        self._elems_list = self.devices_treeView.model().get_elements("selected")
+        #model = self.devices_treeView.model()
+        #self.devicesChanged.emit(model._selected_elements)
+
+    @pyqtSlot(list)
+    def on_selected_fields_updated(self, fields):
+        o = self.field_cbb
+        o.clear()
+        o.addItems(fields)
+        if 'X' in fields:
+            # BPM
+            o.setCurrentText('X')
+        elif 'TYP' in fields:
+            # BCM
+            o.setCurrentText('TYP')
+
+    @pyqtSlot('QString')
+    def on_elem_field_changed(self, s):
+        try:
+            model = self.devices_treeView.model()
+            model.change_field(s)
+        except AttributeError:
+            QMessageBox.warning(self, "Change Field",
+                    "Failed to change field.", QMessageBox.Ok)
+
