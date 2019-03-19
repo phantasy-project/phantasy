bfeecc81845e02c34d93ec149bdab0bde18449de
Tong Zhang <zhangt@frib.msu.edu>
2019-03-19 11:06:59 -0400

    ENH/DV: Add updated ElementListModel for listing devices.

--- phantasy/apps/diag_viewer/utils.py
+++ phantasy/apps/diag_viewer/utils.py
@@ -1,32 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from PyQt5.QtCore import QObject
-from PyQt5.QtCore import QVariant
-from PyQt5.QtCore import pyqtSignal
+from functools import partial
+from PyQt5.QtWidgets import QComboBox
+from PyQt5.QtWidgets import QToolButton
+from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QSize
+from phantasy.apps.trajectory_viewer.utils import ElementListModel
+from phantasy.apps.trajectory_viewer.ui import details_icon
 
-from phantasy.recipes import get_devices_by_type
 
+class ElementListModelDV(ElementListModel):
+    def __init__(self, parent, mp, enames, **kws):
+        ElementListModel.__init__(self, parent, mp, enames, **kws)
 
-class DeviceLoader(QObject):
-    # true: successful, false: failed to load lattice
-    finished = pyqtSignal(bool)
-    results_ready = pyqtSignal(QVariant, QVariant)
-
-    def __init__(self, machine, segments, dtype, parent=None):
-        super(DeviceLoader, self).__init__(parent)
-        self._parent = parent
-        self._mach = machine
-        self._segs = segments
-        self._dtype = dtype
-
-    def run(self):
+    def set_columns(self):
+        v = self._v
+        for i, ename in enumerate(self._enames):
+            # fields
+            elem = self.name_elem_map[ename]
+            cbb = QComboBox()
+            cbb.addItems(elem.fields)
+            v.setIndexWidget(self.index(i, self.i_field), cbb)
+            elem_item = self.item(i, self.i_name)
+            cbb.currentTextChanged.connect(
+                    partial(self.on_field_changed, elem_item))
+            # info
+            btn = QToolButton()
+            btn.setIcon(QIcon(QPixmap(details_icon)))
+            btn.setIconSize(QSize(24, 24))
+            btn.setToolTip("Show details of {}.".format(ename))
+            v.setIndexWidget(self.index(i, self.i_info), btn)
+            btn.clicked.connect(partial(self.show_elem_info, elem))
         try:
-            names, elems = get_devices_by_type(self._dtype, machine=self._mach,
-                                           segments=self._segs)
+            # emit list of element fields
+            self.fieldsSelected.emit(elem.fields)
         except:
-            self.results_ready.emit(None, None)
-            self.finished.emit(False)
-        else:
-            self.results_ready.emit(names, elems)
-            self.finished.emit(True)
+            print("No selected fields.")
