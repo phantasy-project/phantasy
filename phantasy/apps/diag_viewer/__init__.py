141014edd439f58bc270289d0e32c8482fe3a64e
Tong Zhang <zhangt@frib.msu.edu>
2019-03-20 16:46:57 -0400

    MNT/DV: Update title and bump up verion to 1.0.

--- phantasy/apps/diag_viewer/__init__.py
+++ phantasy/apps/diag_viewer/__init__.py
@@ -9,14 +9,14 @@ __authors__ = "Tong Zhang"
 __copyright__ = "(c) 2019, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
-__version__ = '0.1'
+__version__ = '1.0'
 
 
 def run(cli=False):
     app = QApplication(sys.argv)
     w = DeviceViewerWindow(version=__version__)
     w.show()
-    w.setWindowTitle("Devices Viewer")
+    w.setWindowTitle("Device Viewer: Visualize Device Readings and Settings")
     if cli:
         app.exec_()
     else:
