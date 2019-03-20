13fe4023724c03d448987b72c0ee718444b345dc
Tong Zhang <zhangt@frib.msu.edu>
2019-03-20 16:43:50 -0400

    ENH/DV: Implement the methods for data saving.

--- phantasy/apps/diag_viewer/app_save.py
+++ phantasy/apps/diag_viewer/app_save.py
@@ -2,13 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import os
+import time
+import numpy as np
 
 from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtCore import pyqtSlot
 
 from phantasy.apps.utils import get_save_filename
+from phantasy import epoch2human
+from phantasy.recipes import save_all_settings
 
 from .ui.ui_save import Ui_Dialog
+
+TS_FMT = "%Y-%m-%d %H:%M:%S"
 
 
 class SaveDataDialog(QDialog, Ui_Dialog):
@@ -30,6 +37,7 @@ class SaveDataDialog(QDialog, Ui_Dialog):
         self.save_settings_chkbox.setChecked(True)
 
         # all segs
+        self._mach = "FRIB"
         all_segs = ("LEBT", "MEBT", "MEBT2FS1A", "MEBT2FS1B", )
         self._all_segs_chkbox = [
             getattr(self, "{}_chkbox".format(s.lower())) for s in all_segs]
@@ -56,11 +64,64 @@ class SaveDataDialog(QDialog, Ui_Dialog):
             self._selected_segs.append(s)
         else:
             self._selected_segs.remove(s)
-        print(self._selected_segs)
 
     @pyqtSlot()
     def on_save_data(self):
         print("SaveDataDialog: Save Data")
+        ctime = epoch2human(time.time(), fmt=TS_FMT)
+        self._save_data(timestamp=ctime)
+
+        if self.save_figure_chkbox.isChecked():
+                self._save_figure()
+
+        if self.save_settings_chkbox.isChecked():
+            self._save_settings(timestamp=ctime)
+
+    def _save_data(self, **kws):
+        # save data.
+        try:
+            filepath = self.filepath_lineEdit.text()
+            data = self.parent.data
+            header = ','.join(i.name for i in self.parent._elems_list)
+            comments = "# "
+            for k, v in kws.items():
+                comments += "{}: {}".format(k, v)
+            comments += "\n"
+            np.savetxt(filepath, data, delimiter=",", header=header, comments=comments)
+        except:
+            QMessageBox.warning(self, "Save Data Warning",
+                    "Failed to save data to {}.".format(filepath), QMessageBox.Ok)
+        else:
+            QMessageBox.information(self, "Save Data",
+                    "Saved data to {}.".format(filepath), QMessageBox.Ok)
+
+    def _save_figure(self):
+        # save figure
+        try:
+            o = self.parent.matplotlibbarWidget
+            filepath = self.figure_filepath_lineEdit.text()
+            opt = {}
+            opt.update({'dpi': o.getFigureDpi()})
+            o.figure.savefig(filepath, **opt)
+        except:
+            QMessageBox.warning(self, "Save Figure Warning",
+                    "Failed to save figure to {}.".format(filepath), QMessageBox.Ok)
+        else:
+            QMessageBox.information(self, "Save Figure",
+                    "Saved figure to {}.".format(filepath), QMessageBox.Ok)
+
+    def _save_settings(self, **kws):
+        # save settings
+        try:
+            filepath = self.settings_filepath_lineEdit.text()
+            save_all_settings(filepath, segments=self._selected_segs,
+                              machine=self._mach, **kws)
+        except:
+            QMessageBox.warning(self, "Save Settings Warning",
+                    "Failed to save settings to {}.".format(filepath), QMessageBox.Ok)
+        else:
+            QMessageBox.information(self, "Save Settings",
+                    "Saved settings to {}.".format(filepath), QMessageBox.Ok)
 
     @pyqtSlot()
     def on_get_filepath(self):
