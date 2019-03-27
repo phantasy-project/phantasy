464808d049b9a53fe997a9c212a06b6b9490e500
Tong Zhang <zhangt@frib.msu.edu>
2019-03-27 14:40:46 -0400

    MNT/DV: Set up default --mpl-style option.

--- phantasy/apps/diag_viewer/__init__.py
+++ phantasy/apps/diag_viewer/__init__.py
@@ -1,9 +1,14 @@
 # -*- coding: utf8 -*-
 
+import matplotlib.style as mplstyle
+
 import sys
 from phantasy_ui import QApp as QApplication
 
 from .app import DeviceViewerWindow
+
+DEFALT_MSTYLE = 'fivethirtyeight'
+
 
 __authors__ = "Tong Zhang"
 __copyright__ = "(c) 2019, Facility for Rare Isotope beams," \
@@ -13,6 +18,13 @@ __version__ = '1.1'
 
 
 def run(cli=False):
+    arg = sys.argv
+    mstyle = DEFALT_MSTYLE
+    if '--mpl-style' in arg:
+        mstyle = arg[arg.index('--mpl-style') + 1]
+        if mstyle not in mplstyle.available:
+            mstyle = DEFALT_MSTYLE
+    mplstyle.use(mstyle)
     app = QApplication(sys.argv)
     w = DeviceViewerWindow(version=__version__)
     w.show()
