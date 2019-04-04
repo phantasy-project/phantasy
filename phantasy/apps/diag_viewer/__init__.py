e37122fcdb0beb6b75e33cd940d3d931ea8535d0
Tong Zhang <zhangt@frib.msu.edu>
2019-04-04 10:47:25 -0400

    MNT/DV: Bump up version to 2.1.

--- phantasy/apps/diag_viewer/__init__.py
+++ phantasy/apps/diag_viewer/__init__.py
@@ -11,7 +11,7 @@ __copyright__ = "(c) 2019, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
 __title__ = "Device Viewer: Visualize Device Readings and Settings"
-__version__ = '2.0'
+__version__ = '2.1'
 
 
 def run(cli=False):
