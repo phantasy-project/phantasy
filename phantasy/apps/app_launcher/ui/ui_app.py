f1f17610c4384f80e1bba4dfa2e46c28e0a5f820
Tong Zhang <zhangt@frib.msu.edu>
2019-04-04 18:01:07 -0400

    UI/AL: Update UI and resources.

--- phantasy/apps/app_launcher/ui/ui_app.py
+++ phantasy/apps/app_launcher/ui/ui_app.py
@@ -36,31 +36,48 @@ class Ui_MainWindow(object):
         self.gridLayout.addWidget(self.tableView, 2, 0, 1, 1)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setContentsMargins(-1, 0, -1, -1)
-        self.horizontalLayout.setSpacing(4)
+        self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setObjectName("horizontalLayout")
+        self.about_btn = QtWidgets.QToolButton(self.centralwidget)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(
+            QtGui.QPixmap(":/icons/frib.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.about_btn.setIcon(icon1)
+        self.about_btn.setIconSize(QtCore.QSize(64, 64))
+        self.about_btn.setAutoRaise(True)
+        self.about_btn.setObjectName("about_btn")
+        self.horizontalLayout.addWidget(self.about_btn)
         self.title_lbl = QtWidgets.QLabel(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                           QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.title_lbl.sizePolicy().hasHeightForWidth())
+        self.title_lbl.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        font.setBold(True)
+        font.setWeight(75)
+        self.title_lbl.setFont(font)
         self.title_lbl.setStyleSheet("QLabel {\n"
-                                     "    border: 0.5px solid gray;\n"
+                                     "    font-weight: bold;\n"
+                                     "    font-size: 20pt;\n"
+                                     "    /*border: 0.5px solid gray;\n"
                                      "    border-radius: 3px;\n"
                                      "    padding: 5px 10px 5px 10px;\n"
                                      "    background-color: white;\n"
                                      "    color: blue;\n"
+                                     "    */\n"
                                      "}")
         self.title_lbl.setText("")
         self.title_lbl.setAlignment(QtCore.Qt.AlignCenter)
         self.title_lbl.setObjectName("title_lbl")
         self.horizontalLayout.addWidget(self.title_lbl)
-        self.enable_debug_btn = QtWidgets.QToolButton(self.centralwidget)
-        icon1 = QtGui.QIcon()
-        icon1.addPixmap(
-            QtGui.QPixmap(":/icons/log.png"), QtGui.QIcon.Normal,
-            QtGui.QIcon.Off)
-        self.enable_debug_btn.setIcon(icon1)
-        self.enable_debug_btn.setIconSize(QtCore.QSize(24, 24))
-        self.enable_debug_btn.setCheckable(True)
-        self.enable_debug_btn.setAutoRaise(True)
-        self.enable_debug_btn.setObjectName("enable_debug_btn")
-        self.horizontalLayout.addWidget(self.enable_debug_btn)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName("verticalLayout")
         self.show_log_btn = QtWidgets.QToolButton(self.centralwidget)
         self.show_log_btn.setEnabled(False)
         icon2 = QtGui.QIcon()
@@ -71,11 +88,23 @@ class Ui_MainWindow(object):
         self.show_log_btn.setIconSize(QtCore.QSize(24, 24))
         self.show_log_btn.setAutoRaise(True)
         self.show_log_btn.setObjectName("show_log_btn")
-        self.horizontalLayout.addWidget(self.show_log_btn)
+        self.verticalLayout.addWidget(self.show_log_btn)
+        self.enable_debug_btn = QtWidgets.QToolButton(self.centralwidget)
+        icon3 = QtGui.QIcon()
+        icon3.addPixmap(
+            QtGui.QPixmap(":/icons/log.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.enable_debug_btn.setIcon(icon3)
+        self.enable_debug_btn.setIconSize(QtCore.QSize(24, 24))
+        self.enable_debug_btn.setCheckable(True)
+        self.enable_debug_btn.setAutoRaise(True)
+        self.enable_debug_btn.setObjectName("enable_debug_btn")
+        self.verticalLayout.addWidget(self.enable_debug_btn)
+        self.horizontalLayout.addLayout(self.verticalLayout)
         self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1000, 34))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1000, 32))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
 
@@ -87,17 +116,19 @@ class Ui_MainWindow(object):
         self.show_log_btn.clicked.connect(MainWindow.on_show_log)
         self.enable_debug_btn.toggled['bool'].connect(
             self.show_log_btn.setEnabled)
+        self.about_btn.clicked.connect(MainWindow.onAbout)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.enable_debug_btn.setToolTip(
-            _translate("MainWindow", "Enable log messages capture"))
-        self.enable_debug_btn.setText(_translate("MainWindow", "Debug"))
+        self.about_btn.setText(_translate("MainWindow", "..."))
         self.show_log_btn.setToolTip(
             _translate("MainWindow", "Show log messages"))
         self.show_log_btn.setText(_translate("MainWindow", "Show Log"))
+        self.enable_debug_btn.setToolTip(
+            _translate("MainWindow", "Enable log messages capture"))
+        self.enable_debug_btn.setText(_translate("MainWindow", "Debug"))
 
 
 from . import resources_rc
