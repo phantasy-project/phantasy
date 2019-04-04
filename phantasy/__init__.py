d70e14176a52688dd768047cc4e94ba23decc91d
Tong Zhang <zhangt@frib.msu.edu>
2019-04-04 19:07:27 -0400

    MNT: Update package docstrings.

--- phantasy/__init__.py
+++ phantasy/__init__.py
@@ -30,7 +30,7 @@ from phantasy.library import *
 
 
 __authors__ = "Tong Zhang"
-__copyright__ = "(c) 2016-2018, Facility for Rare Isotope beams," \
+__copyright__ = "(c) 2016-2019, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
 __version__ = "1.10.0"
@@ -39,18 +39,16 @@ __doc__ = \
 """PHANTASY: [P]hysics [H]igh-level [A]pplications a[N]d [T]oolkit for
 [A]ccelerator [SY]stem.
 
-Main features of ``phantasy``:
+PHANTASY features:
 
-- Creating virtual accelerators, EPICS controls environment;
-- Modeling and tuning accelerator on high-level computing stage;
-- Device abstraction and configuration management;
-- Online modeling, integrating physics and engineering units scaling laws;
+- EPICS-based virtual accelerators system environment
+- Online modeling, integrating physics and engineering units scaling laws
+- Device abstraction and configuration management
+- Interactive Python scripting controls to the accelerator system
+- GUI apps for commissioning and operation
 
-Physics applications built on top of ``phantasy`` are developed as another
-repo: ``phantasy-apps``, whose names in Debian repo are prefixed with
-``python-``, i.e. ``python-phantasy`` and ``python-phantasy-apps``.
-
-More details see documentation at https://archman.github.io/phantasy/.
+For Debian 8 and 9, meta package: `phantasy' contain all the software
+product from this development.
 
 :version: %s
 :authors: %s
