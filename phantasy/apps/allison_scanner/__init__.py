d270ae48fb3dedcb9fdaf5c90b8574c435d298db
Tong Zhang <zhangt@frib.msu.edu>
2019-04-09 09:33:27 -0400

    MNT/AS: Update start up module.

--- phantasy/apps/allison_scanner/__init__.py
+++ phantasy/apps/allison_scanner/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf8 -*-
 
 import sys
-from PyQt5.QtWidgets import QApplication
+from phantasy_ui import QApp as QApplication
+from phantasy_ui import set_mplstyle
 
 from .app import AllisonScannerWindow
 
@@ -9,14 +10,16 @@ __authors__ = "Tong Zhang"
 __copyright__ = "(c) 2019, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
+__title__ = "Allison Scanner: Beam Twiss Parameters Measurement"
 __version__ = '0.1'
 
 
 def run(cli=False):
+    set_mplstyle(sys.argv)
     app = QApplication(sys.argv)
     w = AllisonScannerWindow(version=__version__)
     w.show()
-    w.setWindowTitle("Allison Scanner Application")
+    w.setWindowTitle(__title__)
     if cli:
         app.exec_()
     else:
