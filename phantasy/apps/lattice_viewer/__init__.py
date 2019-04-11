b1d0cc088fdd10609c2fadf6fccbb0eb0ab8c6ed
Tong Zhang <zhangt@frib.msu.edu>
2019-04-11 10:10:42 -0400

    MNT/LV: Update run function.

--- phantasy/apps/lattice_viewer/__init__.py
+++ phantasy/apps/lattice_viewer/__init__.py
@@ -9,13 +9,14 @@ __authors__ = "Tong Zhang"
 __copyright__ = "(c) 2018, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
+__title__ = "Lattice Viewer: Investigate Loaded Lattice"
 __version__ = '1.1'
 
 
 def run(cli=False):
     app = QApplication(sys.argv)
     w = LatticeViewerWindow(version=__version__)
-    w.setWindowTitle("FRIB Lattice Viewer")
+    w.setWindowTitle(__title__)
     w.show()
     if cli:
         app.exec_()
