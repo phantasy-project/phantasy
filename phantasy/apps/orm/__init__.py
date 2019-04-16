a9abf09d2a09c6b403507d9ee8c36c4b39bea27b
Tong Zhang <zhangt@frib.msu.edu>
2019-04-16 14:45:13 -0400

    MNT/ORM: Bump up version to 3.2.

--- phantasy/apps/orm/__init__.py
+++ phantasy/apps/orm/__init__.py
@@ -6,11 +6,11 @@ from phantasy_ui import QApp as QApplication
 from .app import OrbitResponseMatrixWindow
 
 __authors__ = "Tong Zhang"
-__copyright__ = "(c) 2018, Facility for Rare Isotope beams," \
+__copyright__ = "(c) 2018-2019, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
 __title__ = "Beam Central Trajectory Response Matrix"
-__version__ = '3.1'
+__version__ = '3.2'
 
 
 def run(cli=False):
