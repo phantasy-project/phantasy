1225066ddd7b4f1ca740177be2f47054340f0e3d
Tong Zhang <zhangt@frib.msu.edu>
2019-04-18 16:03:49 -0400

    UI/IV: Update UIs and resources.

--- phantasy/apps/imageviewer/ui/ui_app.py
+++ phantasy/apps/imageviewer/ui/ui_app.py
@@ -15,6 +15,49 @@ class Ui_MainWindow(object):
         MainWindow.resize(1920, 1440)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.splitter = QtWidgets.QSplitter(self.centralwidget)
+        self.splitter.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter.setObjectName("splitter")
+        self.groupBox = QtWidgets.QGroupBox(self.splitter)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                           QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.groupBox.sizePolicy().hasHeightForWidth())
+        self.groupBox.setSizePolicy(sizePolicy)
+        self.groupBox.setObjectName("groupBox")
+        self.gridLayout = QtWidgets.QGridLayout(self.groupBox)
+        self.gridLayout.setObjectName("gridLayout")
+        self.matplotlibimageWidget = MatplotlibImageWidget(self.groupBox)
+        self.matplotlibimageWidget.setObjectName("matplotlibimageWidget")
+        self.gridLayout.addWidget(self.matplotlibimageWidget, 0, 0, 1, 1)
+        self.groupBox_2 = QtWidgets.QGroupBox(self.splitter)
+        self.groupBox_2.setObjectName("groupBox_2")
+        self.gridLayout_3 = QtWidgets.QGridLayout(self.groupBox_2)
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.wf_pvname_lineEdit = QtWidgets.QLineEdit(self.groupBox_2)
+        self.wf_pvname_lineEdit.setObjectName("wf_pvname_lineEdit")
+        self.gridLayout_3.addWidget(self.wf_pvname_lineEdit, 1, 0, 1, 3)
+        self.ydim_lineEdit = QtWidgets.QLineEdit(self.groupBox_2)
+        self.ydim_lineEdit.setObjectName("ydim_lineEdit")
+        self.gridLayout_3.addWidget(self.ydim_lineEdit, 2, 2, 1, 1)
+        self.label_2 = QtWidgets.QLabel(self.groupBox_2)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout_3.addWidget(self.label_2, 2, 0, 1, 1)
+        self.label = QtWidgets.QLabel(self.groupBox_2)
+        self.label.setObjectName("label")
+        self.gridLayout_3.addWidget(self.label, 0, 0, 1, 2)
+        self.xdim_lineEdit = QtWidgets.QLineEdit(self.groupBox_2)
+        self.xdim_lineEdit.setObjectName("xdim_lineEdit")
+        self.gridLayout_3.addWidget(self.xdim_lineEdit, 2, 1, 1, 1)
+        spacerItem = QtWidgets.QSpacerItem(20, 40,
+                                           QtWidgets.QSizePolicy.Minimum,
+                                           QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout_3.addItem(spacerItem, 3, 2, 1, 1)
+        self.gridLayout_2.addWidget(self.splitter, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1920, 34))
@@ -60,6 +103,10 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.groupBox.setTitle(_translate("MainWindow", "Data Visualization"))
+        self.groupBox_2.setTitle(_translate("MainWindow", "Controls"))
+        self.label_2.setText(_translate("MainWindow", "Dims"))
+        self.label.setText(_translate("MainWindow", "Data Source"))
         self.menu_File.setTitle(_translate("MainWindow", "&File"))
         self.menu_Help.setTitle(_translate("MainWindow", "&Help"))
         self.actionE_xit.setText(_translate("MainWindow", "E&xit"))
@@ -67,6 +114,7 @@ class Ui_MainWindow(object):
         self.actionAbout_Qt.setText(_translate("MainWindow", "About Qt"))
 
 
+from mpl4qt.widgets.mplimagewidget import MatplotlibImageWidget
 from . import resources_rc
 
 if __name__ == "__main__":
