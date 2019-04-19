88c143e45956cdf6f0c65949e95918745e3712b1
Tong Zhang <zhangt@frib.msu.edu>
2019-04-19 11:56:11 -0400

    UI/AS: Update UI.

--- phantasy/apps/allison_scanner/ui/ui_app.py
+++ phantasy/apps/allison_scanner/ui/ui_app.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui_app.ui'
 #
-# Created by: PyQt5 UI code generator 5.5.1
+# Created by: PyQt5 UI code generator 5.10.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,22 +12,62 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1600, 1400)
+        MainWindow.resize(1920, 1440)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1600, 30))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1920, 34))
         self.menubar.setObjectName("menubar")
+        self.menu_File = QtWidgets.QMenu(self.menubar)
+        self.menu_File.setObjectName("menu_File")
+        self.menu_Help = QtWidgets.QMenu(self.menubar)
+        self.menu_Help.setObjectName("menu_Help")
         MainWindow.setMenuBar(self.menubar)
+        self.actionE_xit = QtWidgets.QAction(MainWindow)
+        icon = QtGui.QIcon()
+        icon.addPixmap(
+            QtGui.QPixmap(":/icons/exit.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.actionE_xit.setIcon(icon)
+        self.actionE_xit.setObjectName("actionE_xit")
+        self.actionAbout = QtWidgets.QAction(MainWindow)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(
+            QtGui.QPixmap(":/icons/info.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.actionAbout.setIcon(icon1)
+        self.actionAbout.setObjectName("actionAbout")
+        self.actionAbout_Qt = QtWidgets.QAction(MainWindow)
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(
+            QtGui.QPixmap(":/icons/qt.png"), QtGui.QIcon.Normal,
+            QtGui.QIcon.Off)
+        self.actionAbout_Qt.setIcon(icon2)
+        self.actionAbout_Qt.setObjectName("actionAbout_Qt")
+        self.menu_File.addAction(self.actionE_xit)
+        self.menu_Help.addAction(self.actionAbout)
+        self.menu_Help.addAction(self.actionAbout_Qt)
+        self.menubar.addAction(self.menu_File.menuAction())
+        self.menubar.addAction(self.menu_Help.menuAction())
 
         self.retranslateUi(MainWindow)
+        self.actionAbout.triggered.connect(MainWindow.onAbout)
+        self.actionAbout_Qt.triggered.connect(MainWindow.onAboutQt)
+        self.actionE_xit.triggered.connect(MainWindow.close)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.menu_File.setTitle(_translate("MainWindow", "&File"))
+        self.menu_Help.setTitle(_translate("MainWindow", "&Help"))
+        self.actionE_xit.setText(_translate("MainWindow", "E&xit"))
+        self.actionAbout.setText(_translate("MainWindow", "About"))
+        self.actionAbout_Qt.setText(_translate("MainWindow", "About Qt"))
 
+
+from . import resources_rc
 
 if __name__ == "__main__":
     import sys
