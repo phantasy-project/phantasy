678b5bfdc21633bdf9910c45e6e9c51ae6c030c0
Tong Zhang <zhangt@frib.msu.edu>
2019-05-03 20:00:46 -0400

    MNT/AS: Update run function.

--- phantasy/apps/allison_scanner/__init__.py
+++ phantasy/apps/allison_scanner/__init__.py
@@ -2,7 +2,7 @@
 
 import sys
 
-import cothread
+#import cothread
 from phantasy_ui import QApp as QApplication
 from phantasy_ui import set_mplstyle
 
@@ -19,10 +19,15 @@ __title__ = "Allison Scanner: Beam Twiss Parameters Measurement"
 __version__ = '0.1'
 
 
-def run():
+def run(cli=False):
     set_mplstyle(sys.argv)
-    QApplication(sys.argv)()
+    # QApplication(sys.argv)()
+    app = QApplication(sys.argv)
     w = AllisonScannerWindow(version=__version__)
     w.show()
     w.setWindowTitle(__title__)
-    cothread.WaitForQuit()
+    # cothread.WaitForQuit()
+    if cli:
+        app.exec_()
+    else:
+        sys.exit(app.exec_())
