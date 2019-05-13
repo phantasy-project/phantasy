6b082dbc8e21bda7d353559a87749ef767b28756
Tong Zhang <zhangt@frib.msu.edu>
2019-05-13 16:29:10 -0400

    ENH/AS: Add function for testing if point in an ellipse or not.

--- phantasy/apps/allison_scanner/utils.py
+++ phantasy/apps/allison_scanner/utils.py
@@ -4,6 +4,8 @@
 import os
 from collections import OrderedDict
 
+import numpy as np
+
 from phantasy import MachinePortal
 
 
@@ -32,3 +34,28 @@ def get_all_devices(machine="FRIB", segment="LEBT", type="EMS"):
     elems = mp.get_elements(type=type)
     r = [(i.name, i) for i in sorted(elems, key=lambda x:x.name[-4:])]
     return OrderedDict(r)
+
+
+def point_in_ellipse(x, y, ellipse, factor=1.0):
+    """Test if point `(x, y)` in *ellipse* or not.
+
+    Parameters
+    ----------
+    x : float
+        Point x coord.
+    y : float
+        Point y coord.
+    ellipse :
+        Ellipse patch.
+    factor : float
+        Increase/shrink the ellipse by applying coef onto both width and
+        eight, default is 1.0.
+    """
+    x0, y0 = ellipse.center
+    w, h = ellipse.width, ellipse.height
+    theta = np.deg2rad(ellipse.angle)
+    x -= x0
+    y -= y0
+    x1 = np.cos(theta) * x + np.sin(theta) * y
+    y1 = -np.sin(theta) * x + np.cos(theta) * y
+    return (x1 * x1 / w / w + y1 * y1 / h / h) <= 0.25 * factor * factor
