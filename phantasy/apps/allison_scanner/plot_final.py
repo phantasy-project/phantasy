4698be9215c5e02bdf588d9789ed1b74c34809af
Tong Zhang <zhangt@frib.msu.edu>
2019-05-16 14:43:57 -0400

    ENH/AS: Add widget for showing finalized results.

--- phantasy/apps/allison_scanner/plot_final.py
+++ phantasy/apps/allison_scanner/plot_final.py
@@ -34,3 +34,21 @@ class PlotResults(QWidget, Ui_Form):
         self._data.plot(m, results=self._r, ax=self._ax, image_on=False,
                 profile_on=True, profile_opt={'lw': 1.5, 'color': 'w'},
                 ellipse_on=True, ellipse_opt={'c': 'w', 'color': 'w'})
+        # results
+        self._show_results(self._r)
+
+    def _show_results(self, r):
+        for k in r:
+            if k.startswith('alpha'):
+                u = k[-1]
+                break
+
+        ks = '{u}_cen,{u}p_cen,{u}_rms,{u}p_rms,alpha_{u},beta_{u},gamma_{u},emit_{u},emitn_{u}'.format(u=u).split(',')
+        names = ["{}<sub>{}</sub>".format(i, j) for (i, j) in
+                 zip((u, u + "'", '&sigma;', '&sigma;', '&alpha;',
+                     '&beta;', '&gamma;', '&epsilon;', '&epsilon;'),
+                     (0, 0, u, u + "'", u, u, u, u, u + '<sup>n</sup>',))]
+        us = ("mm", "mrad", "mm", "mrad", "", "m", "m<sup>-1</sup>", "mm&middot;mrad", "mm&middot;mrad")
+
+        s =['<h5>{0:<3s} = {1:.6f} {2}<h5>'.format(n, r.get(k), ui) for (n, k, ui) in zip(names, ks, us)]
+        self.textEdit.setHtml("<html>{}</html>".format(''.join(s)))
