9912df9915856e5ec16256d589c0f7372fb8c01f
Tong Zhang <zhangt@frib.msu.edu>
2020-01-24 09:52:37 -0500

    MNT: Move get_settings_from_element_list() to settings subpackage.

--- phantasy/library/settings/__init__.py
+++ phantasy/library/settings/__init__.py
@@ -2,10 +2,12 @@ from .common import Settings
 from .common import snp2dict
 from .common import get_element_settings
 from .common import generate_settings
+from .common import get_settings_from_element_list
 from .flame import build_settings as build_flame_settings
 from .impact import build_settings as build_impact_settings
 
 __all__ = [
     'Settings', 'build_flame_settings', 'build_impact_settings',
     'snp2dict', 'get_element_settings', 'generate_settings',
+    'get_settings_from_element_list',
 ]
