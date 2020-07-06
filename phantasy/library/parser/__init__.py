0919f8ab509ebf7973acb5d55b231250eb71c70f
Tong Zhang <zhangt@frib.msu.edu>
2020-07-06 16:42:21 -0400

    ENH: Add function to read device polarity data.

--- phantasy/library/parser/__init__.py
+++ phantasy/library/parser/__init__.py
@@ -1,7 +1,9 @@
 from .config import Configuration
 from .config import find_machine_config
+from .polarity import readfile as read_polarity
 
 __all__ = [
     'Configuration',
     'find_machine_config',
+    'read_polarity',
 ]
---
+++ phantasy/library/parser/polarity.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Read polarity.
+"""
+import csv
+
+
+def readfile(filepath, fmt='csv'):
+    with open(filepath, 'r') as f:
+        data = csv.reader(f, delimiter=',', skipinitialspace=True)
+        next(data)
+        r = {i[0]: int(i[1]) for i in data}
+    return r
