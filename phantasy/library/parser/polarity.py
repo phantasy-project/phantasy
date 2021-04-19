e81f4f7b633394ab67e7348a00058644dc604510
Tong Zhang <zhangt@frib.msu.edu>
2021-04-19 16:18:39 -0400

    REF: Skip line starts with '#' for reading QUAD polarity data.

--- phantasy/library/parser/polarity.py
+++ phantasy/library/parser/polarity.py
@@ -9,5 +9,5 @@ def readfile(filepath, fmt='csv'):
     with open(filepath, 'r') as f:
         data = csv.reader(f, delimiter=',', skipinitialspace=True)
         next(data)
-        r = {i[0]: int(i[1]) for i in data}
+        r = {i[0]: int(i[1]) for i in data if not i[0].startswith("#")}
     return r
