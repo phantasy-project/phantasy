2ef4f2de667660dfcfd5210e858c376161f8fce9
Tong Zhang <zhangt@frib.msu.edu>
2021-10-14 11:46:03 -0400

    FIX: test_phantasy command.
    - use pytest.main() instead of nose.run().

--- phantasy/tests/__init__.py
+++ phantasy/tests/__init__.py
@@ -1,40 +1,17 @@
 # -*- coding: utf-8 -*-
 
-import nose
+import pytest
 import os
 import shutil
-import sys
-
-try:
-    basestring
-except NameError:
-    basestring = str
 
 CWD = os.path.dirname(__file__)
 TMP_DIR = "/tmp/phantasy_data"
 
-def main(argv=None, **kws):
+def main():
     """Run tests for `phantasy` package, accept CLI options when executing
-    `run_phantasy` command.
-
-    Parameters
-    ----------
-    argv : list
-        List of options of `nosetests`.
+    `test_phantasy` command.
     """
-    argv_list = ['--logging-clear-handlers',
-                 '--logging-level=INFO',
-                 '--nologcapture']
-    argv_list.extend(sys.argv[1:])
-    if argv is not None:
-        if isinstance(argv, basestring):
-            argv = argv,
-        argv_list.extend(argv)
-        nose.run(argv=list(set(argv_list)), defaultTest=CWD, **kws)
-    else:
-        nose.run(argv=argv_list, defaultTest=CWD, **kws)
-
+    pytest.main([CWD])
     if os.path.isdir(TMP_DIR):
         shutil.rmtree(TMP_DIR)
-
     return 0
