69a82268565a9b478144b9134bc5eacffb2ad11a
Tong Zhang <zhangt@frib.msu.edu>
2023-10-19 14:08:43 -0400

    MNT: Bump version to 2.2.1.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
         return f.read()
 
 
-_version = "2.2.0"
+_version = "2.2.1"
 _name = "phantasy"
 _description = 'Physics high-level applications and toolkit for accelerator system'
 _long_description = readme() + '\n\n'
