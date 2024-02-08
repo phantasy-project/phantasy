3a720d6248747cfe6a1d5c21c6b63b0867cf936e
Tong Zhang <zhangt@frib.msu.edu>
2024-02-08 11:10:51 -0500

    MNT: Bump version to 2.2.10.

--- phantasy/__init__.py
+++ phantasy/__init__.py
@@ -31,10 +31,10 @@ from phantasy.library import *
 
 
 __authors__ = "Tong Zhang"
-__copyright__ = "(c) 2016-2023, Facility for Rare Isotope beams," \
+__copyright__ = "(c) 2016-2024, Facility for Rare Isotope beams," \
                 " Michigan State University"
 __contact__ = "Tong Zhang <zhangt@frib.msu.edu>"
-__version__ = "2.2.9"
+__version__ = "2.2.10"
 
 __doc__ = \
 """PHANTASY: [P]hysics [H]igh-level [A]pplications a[N]d [T]oolkit for
