a8853e0d3988cbcf7e8fd42383f168dae5657f1e
rea3 <rea3@nscl.msu.edu>
2024-06-12 13:31:51 -0400

    REF: Improved the way of get the common part of SET/RD as the elem name.

--- phantasy/library/pv/element.py
+++ phantasy/library/pv/element.py
@@ -11,6 +11,7 @@ from phantasy.library.misc import epoch2human
 from functools import partial
 from queue import Queue, Empty
 import epics
+import re
 import time
 import numpy as np
 import click
@@ -82,11 +83,15 @@ class PVElement(object):
         """just guess element name.
         """
         a, b = self.get_pv_name('readback'), self.get_pv_name('setpoint')
-        n = set(a.rsplit(':', 1)).intersection(b.rsplit(':', 1))
-        if n:
-            return n.pop()
+        s = ''
+        for i, j in zip(a, b):
+            if i != j: break
+            s += i
+        r = re.findall(r'[a-zA-Z0-9:_]*[a-zA-Z0-9]', s)
+        if r:
+            return r[0]
         else:
-            return 'undefined'
+            return b
 
     @property
     def readback(self):
